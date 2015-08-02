3cd5aa2acbc827918d4b956754a16f262caa0e9e
leios <jrs.schloss@gmail.com>
2015-08-03 06:34:26 +0900

    Added mt19937 rng and populate f(x)'s
    Also added comment about structure of make_list f(x)
    Will work on that f(x) tomorrow!

--- MD/MD.cpp
+++ MD/MD.cpp
@@ -13,27 +13,35 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <random>
 
 using namespace std;
+using simtime = int;
 
 /*----------------------------------------------------------------------------//
 * STRUCTS AND FUNCTIONS
 *-----------------------------------------------------------------------------*/
 
 // Holds our data in a central struct, to be called mainly in a vector
-struct data{
+struct particle{
+    int PID, ts;
     double pos_x, pos_y, pos_z, vel_x, vel_y, vel_z;
 };
 
+// holds interaction data
+struct interact{
+    int ts, part1, part2;
+};
+
 // Makes starting data
-vector <data> populate();
+vector <particle> populate(int pnum, double box_length, double max_vel);
 
 // Makes the list for our simulation later, required starting data
-vector <int> make_list(vector <data> curr_data);
+vector <interact> make_list(vector <vector <particle> > curr_data);
 
 // This performs our MD simulation with a vector of interactions
 // Also writes simulation to file, specified by README
-void simulate(vector<int> interactions, vector <data> curr_data);
+void simulate(vector<int> interactions, vector <particle> curr_data);
 
 /*----------------------------------------------------------------------------//
 * MAIN
@@ -49,22 +57,48 @@ int main(void){
 *-----------------------------------------------------------------------------*/
 
 // Makes starting data
-vector <data> populate(){
-    vector <data> curr_data;
+vector <particle> populate(int pnum, double box_length, double max_vel){
+    vector <particle> curr_data;
+    particle temp;
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<double>dis(0.0, 1.0);
+
+
+    for (int i = 0; i < pnum; i++){
+
+        temp.PID = i;
+        temp.ts = 0;
+
+        temp.pos_x = dis(gen) * box_length;
+        temp.pos_y = dis(gen) * box_length;
+        temp.pos_z = dis(gen) * box_length;
+        temp.vel_x = dis(gen) * max_vel;
+        temp.vel_y = dis(gen) * max_vel;
+        temp.vel_z = dis(gen) * max_vel;
+
+        curr_data.push_back(temp);
+    
+    }
 
     return curr_data;
 }
 
 // Makes the list for our simulation later, required starting data
-vector <int> make_list(vector <data> curr_data){
-    vector <int> list;
+// Step 1: Check interactions between particles, based on README link
+// Step 2: Update list.
+vector <interact> make_list(vector <particle> curr_data){
+    vector <interact> list;
 
     return list;
 }
 
 // This performs our MD simulation with a vector of interactions
 // Also writes simulation to file, specified by README
-void simulate(vector<int> interactions, vector <data> curr_data){
+// Note: Time-loop here
+void simulate(vector<int> interactions, vector <particle> curr_data){
+
 
 }
 
