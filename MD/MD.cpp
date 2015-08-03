b6d9f4736b7acbeee7bfd7833c68df0736b18e8f
leios <jrs.schloss@gmail.com>
2015-08-04 06:58:40 +0900

    Implemented half of our make_list command. Still needs to sort.

--- MD/MD.cpp
+++ MD/MD.cpp
@@ -2,7 +2,7 @@
 *
 *             MD.cpp -- a simple event-driven MD simulation
 *
-* Purpose: Figure out the pressuer on the interior of a box based on
+* Purpose: Figure out the pressure on the interior of a box based on
 *          Boltzmann's theory and stuff
 *
 *   Notes: This simulation will predominantly follow the logic of the link in
@@ -14,11 +14,12 @@
 #include <vector>
 #include <fstream>
 #include <random>
+//#include <algorithm>
 
 namespace sim{
 	/* putting our stuff in its own namespace so we can call "time" "time"
 	   without colliding with names from the standard library */
-	using time = int;
+	using time = double;
 }
 
 /*----------------------------------------------------------------------------//
@@ -34,7 +35,7 @@ struct Particle{
 
 // holds interaction data
 struct Interaction{
-	sim::time ts;
+	sim::time rtime;
 	int part1, part2;
 };
 
@@ -42,7 +43,8 @@ struct Interaction{
 std::vector<Particle> populate(int pnum, double box_length, double max_vel);
 
 // Makes the list for our simulation later, required starting data
-std::vector<Interaction> make_list(const std::vector<Particle> &curr_data);
+std::vector<Interaction> make_list(const std::vector<Particle> &curr_data,
+                                   double box_length, double radius);
 
 // This performs our MD simulation with a vector of interactions
 // Also writes simulation to file, specified by README
@@ -52,8 +54,20 @@ void simulate(std::vector<int> interactions, std::vector<Particle> curr_data);
 * MAIN
 *-----------------------------------------------------------------------------*/
 
-int main(){
-	//no void and no return 0 for main
+int main(void){
+
+    int pnum = 100;
+    double box_length = 10, max_vel = 0.01;
+
+    std::vector<Particle> curr_data = populate(pnum, box_length, max_vel);
+
+    for (auto &p : curr_data){
+
+        std::cout << p.pos_x << std::endl;
+       
+    }
+
+    return 0;
 }
 
 /*----------------------------------------------------------------------------//
@@ -62,46 +76,106 @@ int main(){
 
 // Makes starting data
 std::vector<Particle> populate(int pnum, double box_length, double max_vel){
-	std::vector<Particle> curr_data(pnum); //<- constructor instead of pushing
+    std::vector<Particle> curr_data(pnum);
 
-	static std::random_device rd; //static to create only 1 random_device
-	static std::mt19937 gen(rd());
-	/* instead of doing % and * to get the correct distribution we directly
-	   specify which distribution we want */
-	std::uniform_real_distribution<double> box_length_distribution(0, box_length);
-	std::uniform_real_distribution<double> max_vel_distribution(0, max_vel);
+    // static to create only 1 random_device
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
 
-	int PID_counter = 0;
-	for (auto &p : curr_data){ //read: for all particles p in curr_data
-		p.PID = PID_counter++;
-		p.ts = 0;
+    /* instead of doing % and * to get the correct distribution we directly
+       specify which distribution we want */
 
-		/* maybe you like this version more than the loop below
-		p.pos_x = box_length_distribution(gen);
-		p.pos_y = box_length_distribution(gen);
-		p.pos_z = box_length_distribution(gen);
-		p.vel_x = max_vel_distribution(gen);
-		p.vel_y = max_vel_distribution(gen);
-		p.vel_z = max_vel_distribution(gen);
-		*/
-		for (auto &pos : { &p.pos_x, &p.pos_y, &p.pos_z })
-			*pos = box_length_distribution(gen);
-		for (auto &vel : { &p.vel_x, &p.vel_y, &p.vel_z })
-			*vel = max_vel_distribution(gen);
-	}
+    std::uniform_real_distribution<double> 
+        box_length_distribution(0, box_length);
+    std::uniform_real_distribution<double> 
+       max_vel_distribution(0, max_vel);
 
-	return curr_data;
+    int PID_counter = 0;
+    for (auto &p : curr_data){ //read: for all particles p in curr_data
+        p.ts = 0;
+        p.PID = PID_counter++;
+
+        for (auto &pos : { &p.pos_x, &p.pos_y, &p.pos_z })
+           *pos = box_length_distribution(gen);
+        for (auto &vel : { &p.vel_x, &p.vel_y, &p.vel_z })
+           *vel = max_vel_distribution(gen);
+    }
+
+return curr_data;
 }
 
 // Makes the list for our simulation later, required starting data
 // Step 1: Check interactions between Particles, based on README link
 // Step 2: Update list.
-std::vector<Interaction> make_list(const std::vector<Particle> &curr_data){
-	/* passing curr_data as const reference to avoid the copy and accidental
-	   overwriting */
-	std::vector<Interaction> list;
+// Step 3: Sort list, lowest first
+std::vector<Interaction> make_list(const std::vector<Particle> &curr_data,
+                                   double box_length, double radius){
+    /* passing curr_data as const reference to avoid the copy and accidental
+    overwriting */
+    std::vector<Interaction> list;
+    Interaction test;
+    int i = 0,j = 0;
 
-	return list;
+    // Step 1 -- find interactions
+    for (auto &ip : curr_data){
+        for (auto &jp : curr_data){
+            double del_x = ip.pos_x - jp.pos_x;
+            double del_y = ip.pos_y - jp.pos_y;
+            double del_z = ip.pos_z - jp.pos_y;
+
+            double del_vx = ip.vel_x - jp.vel_y;
+            double del_vy = ip.vel_y - jp.vel_y;
+            double del_vz = ip.vel_z - jp.vel_z;
+
+            double r_prime = 2 * radius;
+
+            double rad_d = (pow(del_vx * del_x + del_vy * del_y 
+                                 + del_vz * del_z, 2)
+                    - 4 * (del_vx * del_vx + del_vy * del_vy + del_vz * del_vz)
+                    * (del_x * del_x + del_y * del_y + del_z * del_z 
+                       - r_prime * r_prime));
+
+            sim::time check;
+            if (del_x * del_vx >= 0 && del_y * del_vy >= 0 &&
+                del_z * del_vz >= 0){
+                check = 0;
+            }
+
+            else if (rad_d > 0){
+                check = 0;
+            }
+
+            else {
+                check = (-(del_vx * del_x + del_vy * del_y + del_vz * del_z) 
+                        + sqrt(rad_d)) / (2 * (del_vx * del_vx + del_vz * del_vz
+                        + del_vy * del_vy));
+            }
+
+
+            // Step 2 -- update list
+            if (check != 0){
+                test.rtime = check;
+                test.part1 = i;
+                test.part2 = j;
+                list.push_back(test);
+            }
+            j++;
+        }
+        i++;
+    }
+
+    // Step 3 -- sort the list
+/*
+    // std::sort()
+    vector <Interaction> dummy;
+
+    for (auto &ele : list){
+    }
+
+std::sort(std::begin(vec), std::end(vec), [](const Particle 
+                &lhs, const Particle &rhs){ return lhs.ts < rhs.ts; });
+*/
+    return list;
 }
 
 // This performs our MD simulation with a vector of interactions
