d811408eebe91c222f0c0cd9aba945d943ae9a52
leios <jrs.schloss@gmail.com>
2015-08-26 06:26:24 +0900

    added movement of later ant generations.

--- ants/ant.cpp
+++ ants/ant.cpp
@@ -15,9 +15,6 @@
 * STRUCTS
 *-----------------------------------------------------------------------------*/
 
-template <typename T, size_t rows, size_t cols>
-using array2d = std::array<std::array<T, cols>, rows>;
-
 // grid size
 const int n = 5;
 
@@ -26,6 +23,11 @@ struct coord{
     int x, y;
 };
 
+// Template for 2d boolean arrays 
+template <typename T, size_t rows, size_t cols>
+using array2d = std::array<std::array<T, cols>, rows>;
+
+// defines operators for definitions above
 inline bool operator==(const coord& lhs, const coord& rhs) {
     return lhs.x == rhs.x && lhs.y == rhs.y;
 }
@@ -35,6 +37,7 @@ inline bool operator!=(const coord& lhs, const coord& rhs) {
 }
 
 struct ant{
+    array2d<bool, n, n> phetus;
     coord pos;
     std::vector<coord> phepath, antpath;
     int step, phenum, pernum;
@@ -82,33 +85,41 @@ ant step(ant curr, grid landscape, coord spawn_point, int gen_flag){
     // determine possible movement
     // up case
     if (last != up) {
-        next_step[pcount] = up;
-        pcount++;
+        if (landscape.wall[up.x][up.y] == 0){
+            next_step[pcount] = up;
+            pcount++;
+        }
     }
 
     // down case
     if (last != down) {
-        next_step[pcount] = down;
-        pcount++;
+        if (landscape.wall[down.x][down.y] == 0){
+            next_step[pcount] = up;
+            pcount++;
+        }
     }
 
     if (last != left) {
-        next_step[pcount] = left;
-        pcount++;
+        if (landscape.wall[left.x][left.y] == 0){
+            next_step[pcount] = up;
+            pcount++;
+        }
     }
 
     // right case
     if (last != right) {
-        next_step[pcount] = right;
-        pcount++;
+        if (landscape.wall[right.x][right.y] == 0){
+            next_step[pcount] = up;
+            pcount++;
+        }
     }
 
     static std::random_device rd;
     auto seed = rd();
     static std::mt19937 gen(seed);
 
-    if (gen_flag == 0){
-        std::uniform_int_distribution<int> ant_distribution(0,3);
+    if (gen_flag == 0 && curr.phetus[curr.pos.x][curr.pos.y] == 0){
+        std::uniform_int_distribution<int> ant_distribution(0,pcount);
 
         next = next_step[ant_distribution(gen)];
 
@@ -116,13 +127,40 @@ ant step(ant curr, grid landscape, coord spawn_point, int gen_flag){
         curr.pos = next;
     }
 
-// WORKING ON THIS TOMORROW
-/*
     else{
 
-        double prob = curr.pernum / curr.phenum;
-        int choice;
+        double prob = curr.pernum / curr.phenum, aprob[pcount], rn, psum = 0;
+        int choice = -1, cthulu;
         std::uniform_real_distribution<double> ant_distribution(0,1);
+
+        // search through phepath to find ant's curr location
+        for (size_t q = 0; q < curr.phepath.size(); q++){
+            if (curr.pos.x == curr.phepath[q].x &&
+                curr.pos.y == curr.phepath[q].y){
+                cthulu = q;
+            }
+        }
+
+        std::uniform_real_distribution<double> ant_ddist(0,1);
+
+        rn = ant_ddist(gen);
+
+        for (size_t q = 0; q < pcount; q++){
+            if (next_step[q].x == curr.phepath[cthulu +1].x &&
+                next_step[q].y == curr.phepath[cthulu +1].y){
+                aprob[q] = prob;
+            }
+            else{
+                aprob[q] = (1 - prob) / (pcount - 1);
+            }
+
+            psum += aprob[q];
+
+            if (rn < psum && choice < 0){
+                choice = q;
+            }
+
+        }
 
         next = next_step[choice];
 
@@ -131,7 +169,6 @@ ant step(ant curr, grid landscape, coord spawn_point, int gen_flag){
 
     }
 
-*/
     return curr;
 
 
