57b7f1e1efd6de2ab6e8064f0619537e6210ba8c
leios <jrs.schloss@gmail.com>
2015-10-19 06:26:13 +0900

    fixed a small bug, accidentally divided by eps

--- inv_lens/FDTD.cpp
+++ inv_lens/FDTD.cpp
@@ -14,7 +14,8 @@
 
 // This is the function we writs the bulk of the code in
 void FDTD(std::vector<double>& Ez, std::vector<double>& Hy,
-          const int final_time, const double eps, std::ofstream& output);
+          const int final_time, const double eps, const int space, 
+          std::ofstream& output);
 
 /*----------------------------------------------------------------------------//
 * MAIN
@@ -31,7 +32,7 @@ int main(){
     // define initial E and H fields
     std::vector<double> Ez(space, 0.0), Hy(space, 0.0);
 
-    FDTD(Ez, Hy, final_time, eps, output);
+    FDTD(Ez, Hy, final_time, eps, space, output);
 
 }
 
@@ -41,27 +42,27 @@ int main(){
 
 // This is the function we writs the bulk of the code in
 void FDTD(std::vector<double>& Ez, std::vector<double>& Hy, 
-          const int final_time, const double eps, std::ofstream& output){
-
-    int space = Hy.size();
+          const int final_time, const double eps, const int space,
+          std::ofstream& output){
 
     // Time looping
     for (size_t t = 0; t < final_time; t++){
 
         // update magnetic field
-        for (size_t dx = 0 ; dx < space - 1; dx++){
+        for (size_t dx = 0; dx < space - 1; dx++){
             Hy[dx] += (Ez[dx + 1] - Ez[dx]) / eps;
         }
 
         // update electric field
         for (size_t dx = 1; dx < space; dx++){
-            Ez[dx] += (Hy[dx] - Hy[dx-1]) / eps;
+            Ez[dx] += (Hy[dx] - Hy[dx-1]) * eps;
+            output << Ez[dx] << '\n';
         }
 
         // set src for next step
         Ez[0] = exp(-(t - 30.0) * (t - 30.0)/100.0);
 
-        output << Ez[50] << '\n';
+        output << '\n' << '\n';
 
     }
 }
