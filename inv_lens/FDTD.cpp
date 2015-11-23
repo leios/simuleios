053285f163f1dbfe09c20653e763bf88f7f1bb7c
leios <jrs.schloss@gmail.com>
2015-11-24 06:49:36 +0900

    removed radial dependence on planewave generation.

--- inv_lens/FDTD.cpp
+++ inv_lens/FDTD.cpp
@@ -17,7 +17,7 @@
 #include <fstream>
 
 static const size_t spacey = 200;
-static const size_t spacex = 1000;
+static const size_t spacex = 500;
 static const size_t losslayer = 20;
 
 struct Bound{
@@ -81,7 +81,7 @@ void FDTD(Field EM,
 double ricker(int time, int loc, double Cour);
 
 // Adding plane wave
-double planewave(int time, int loc, double Cour, int ppw, double radius);
+double planewave(int time, int loc, double Cour, int ppw);
 
 // 2 dimensional functions for E / H movement
 Field Hupdate2d(Field EM, Loss lass, int t);
@@ -110,7 +110,7 @@ int main(){
     // defines output
     std::ofstream output("FDTD.dat", std::ofstream::out);
 
-    int final_time = 3000;
+    int final_time = 2000;
     double eps = 377.0;
 
     // define initial E and H fields
@@ -239,8 +239,8 @@ Field Eupdate1d(Field EM, Loss1d lass1d, int t){
 // Creating loss
 Loss createloss2d(Loss lass, double eps, double Cour, double loss){
 
-    double radius = 150;
-    int sourcex = 0, sourcex2 = 250;
+    double radius = 50;
+    int sourcex = 200, sourcex2 = 250;
     int sourcey = 100, sourcey2 = 100;
     double dist, var, Q, epsp, mup, dist2;
     for (size_t dx = 0; dx < spacex; dx++){
@@ -251,18 +251,22 @@ Loss createloss2d(Loss lass, double eps, double Cour, double loss){
                         + (dy - sourcey2)*(dy - sourcey2)); 
 
             // if (dx > 100 && dx < 150 && dy > 75 && dy < 125){
-            if (dist < radius and dist2 < radius){
+            if (dist < radius){
             // if (dist > 100000000){
-                // Q = cbrt(-(radius / dist) + sqrt((radius/dist) 
-                //                                * (radius/dist) + (1.0/27.0)));
-                // var = (Q - (1.0 / (3.0 * Q))) * (Q - (1.0/ (3.0 * Q)));
-                var = 1.5;
-                if (abs(var) > 2){
-                    var = 2;
+                Q = cbrt(-(radius / dist) + sqrt((radius/dist) 
+                                             * (radius/dist) + (1.0/27.0)));
+                var = (Q - (1.0 / (3.0 * Q))) * (Q - (1.0 / (3.0 * Q)));
+                // var = radius / dist;
+                // var = 1.1;
+                if (var > 3){
+                    var = 3;
+                }
+                if (var < -3){
+                    var = -3;
                 }
 
                 if (isnan(var)){
-                    var = 2;
+                    var = 3;
                 }
                 
                 epsp = eps / (var * var);
@@ -360,8 +364,8 @@ Field TFSF(Field EM, Loss lass, Loss1d lass1d, double Cour){
 
     // TFSF boundary
     Bound first, last;
-    first.x = 10; last.x = 990;
-    first.y = 75; last.y = 125;
+    first.x = 10; last.x = 490;
+    first.y = 10; last.y = 190;
 
     // Update along right edge!
     dx = last.x;
@@ -390,8 +394,8 @@ Field TFSF(Field EM, Loss lass, Loss1d lass1d, double Cour){
     // Insert 1d grid stuff here. Update magnetic and electric field
     Hupdate1d(EM, lass1d, EM.t);
     Eupdate1d(EM, lass1d, EM.t);
-    EM.Ez1d[10] = ricker(EM.t,0, Cour);
-    //EM.Ez1d[10] = planewave(EM.t, 15, Cour, 70, 40);
+    //EM.Ez1d[10] = ricker(EM.t,0, Cour);
+    EM.Ez1d[10] = planewave(EM.t, 15, Cour, 60);
     EM.t++;
     std::cout << EM.t << '\n';
 
@@ -486,7 +490,7 @@ Field ABCcheck(Field EM, Loss lass){
 
 
 // Adding plane wave
-double planewave(int time, int loc, double Cour, int ppw, double radius){
+double planewave(int time, int loc, double Cour, int ppw){
     double plane;
 
     plane = sin((2 * M_PI / (double)ppw) * (Cour * (double)time -
