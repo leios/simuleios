bacddee5b2c8237fb814bba6f121d1eea1b9c7a1
leios <jrs.schloss@gmail.com>
2015-12-22 06:28:39 +0900

    adding in new python file to create better sample data.

--- visualization/evanescent/createsample.py
+++ visualization/evanescent/createsample.py
@@ -15,22 +15,24 @@ import struct
 vfile = open("sample.raw",'wb')
 
 n = 64
-vdata = [[[0 for k in range(n)] for j in range(n)] for i in range(n)]
+vdata = [[[0 for k in range(3*n)] for j in range(3*n)] for i in range(3*n)]
 
 # creating initial gradient for viewing. 0 to 255 at x,y,z = box_length
-for i in range(64):
-    for j in range(64):
-        for k in range(64):
-            vdata[i][j][k] = (0.33 * (i/64) + 0.33 * (j/64) + 0.33 * (k/64))*255
+for q in range(1,4):
+    for i in range(64):
+        for j in range(64):
+            for k in range(64):
+                vdata[i*q][j*q][k*q] = (q-1) * 0.33 * 255
+                #vdata[i*q][j*q][k*q] = (0.11*(q-1)*(i/64) + 0.11*(q-1)*(j/64) + 0.11*(q-1)*(k/64))*255
 
 # function to write data to .raw file for blender
 # note, the density muct be an integer between 0 and 255
 def voxel_gen(vdata, vfile, ii):
-    for i in range(ii):
-        for j in range(ii):
-            for k in range(ii):
-
-                vfile.write(struct.pack('B', abs(int(vdata[i][j][k]))))
+    for q in range(1,4):
+        for i in range(ii):
+            for j in range(ii):
+                for k in range(ii):
+                    vfile.write(struct.pack('B',abs(int(vdata[i*q][j*q][k*q]))))
     vfile.flush()
     vfile.close()
 
