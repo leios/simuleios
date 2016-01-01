7ec65cc9511cc5d4a057944e6b073435ecc21bbe
leios <jrs.schloss@gmail.com>
2016-01-02 07:08:07 +0900

    adding capability to use non-square arrays with ConjGrad.cpp.

--- LA/ConjGrad.cpp
+++ LA/ConjGrad.cpp
@@ -7,6 +7,10 @@
 *              Where A is a square matrix of dimension N and x, b are vectors
 *              of length N. 
 *
+*   Notes: We can write this entire code without std:: vector. Think about it
+*          Need to make sure we are indexing arrays appropriately.
+*          Think about using LAPACK / BLAS
+*
 *-----------------------------------------------------------------------------*/
 
 #include <iostream>
@@ -17,20 +21,20 @@
 struct array2D{
     // initial pointers to array
     std::unique_ptr<double[]> data;
-    size_t n;
+    size_t rows, columns;
 
-    // this defines our interface with array creation
-    array2D(size_t n) : data(new double[n*n]), n(n) {
-        for(size_t i = 0; i < n*n; i++){ data[i] = 0; }
+    // defines our interface with array creation and creates struct memebers
+    array2D(size_t n, size_t m) : data(new double[n*m]), rows(n), columns(m) {
+        for(size_t i = 0; i < n*m; i++){ data[i] = i; }
     }
 
     // These are interfaces for array calls. Basically setting the row length.
     double operator() (size_t row, size_t col) const {
-        return data[row * n + col];
+        return data[row * columns + col];
     }
 
     double& operator() (size_t row, size_t col) {
-        return data[row * n + col];
+        return data[row * columns + col];
     }
 };
 
@@ -47,8 +51,8 @@ std::vector<double>  conjgrad(const array2D &A, const std::vector<double> &b,
 
 int main(){
 
-    array2D test(10);
-    std::cout << test(0,10) << '\n';
+    array2D test(10, 4);
+    std::cout << test(1,4) << '\n';
 }
 
 /*----------------------------------------------------------------------------//
@@ -57,7 +61,8 @@ int main(){
 
 // Smaller function for Matrix multiply
 array2D matmul(const array2D &A, const array2D &B){
-    array2D C(A.n);
+    // Check to make sure we are creating C with an appropriate size
+    array2D C(A.rows, B.columns);
 
     return C;
 }
