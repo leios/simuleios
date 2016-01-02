853adf77bad76be4629502219473b29268f35b9b
leios <jrs.schloss@gmail.com>
2016-01-03 07:02:04 +0900

    added a few more beta functions. Will be tested next stream.

--- LA/ConjGrad.cpp
+++ LA/ConjGrad.cpp
@@ -10,6 +10,8 @@
 *   Notes: We can write this entire code without std:: vector. Think about it
 *          Need to make sure we are indexing arrays appropriately.
 *          Think about using LAPACK / BLAS
+*          matmag is not for matrices yet. Fix that.
+*          ConjGrad function not working or tested yet.
 *
 *-----------------------------------------------------------------------------*/
 
@@ -17,6 +19,7 @@
 #include <vector>
 #include <memory>
 #include <cassert>
+#include <cmath>
 
 // Struct to hold 2d array
 struct array2D{
@@ -44,8 +47,18 @@ struct array2D{
 // Smaller function for Matrix multiply
 array2D matmul(const array2D &A, const array2D &B);
 
+// Function for addition
+array2D matadd(const array2D &A, const array2D &B);
+array2D matdiff(const array2D &A, const array2D &B);
+
+// Function for scaling
+array2D matscale(const array2D &A, double scale);
+
+// Function for finding the magnitude of a vector... Note not array yet.
+double matmag(const array2D &A);
+
 // Function for Conj Gradient -- All the big stuff happens here
-void conjgrad(const array2D &A, const array2D &b, array2D &x);
+void conjgrad(const array2D &A, const array2D &b, array2D &x_0, double thresh);
 
 /*----------------------------------------------------------------------------//
 * MAIN
@@ -54,6 +67,9 @@ void conjgrad(const array2D &A, const array2D &b, array2D &x);
 int main(){
 
     array2D a(2,2), b(2,1);
+
+    a(0,0) = 4; a(1,0) = 1; a(0,1) = 1; a(1,1) = 3;
+    b(0,0) = 2; b(0,1) = 1;
 
     array2D c = matmul(a, b);
 
@@ -92,6 +108,96 @@ array2D matmul(const array2D &A, const array2D &B){
 }
 
 // Function for Conj Gradient -- All the big stuff happens here
-void conjgrad(const array2D &A, const array2D &b, array2D &x){
+
+void conjgrad(const array2D &A, const array2D &b, array2D &x_0, double thresh){
+
+    // Not working. Messily break if someone tries to use it.
+    std::cout << "Not working" << '\n';
+    assert(thresh < 0);
+
+    array2D r(x_0.rows, x_0.columns), p(x_0.rows,x_0.columns);
+    double alpha, diff;
+
+    // definingin first r
+    array2D r_0 = matdiff(b, matmul(A,x_0));
+
+    // setting x arbitrarily high to start
+    array2D x = matscale(x_0, 4);
+
+    // *grumble... gustorn... grumble*
+    array2D p_0 = matscale(r_0,1);
+/*
+    while (diff > thresh){
+    }
+*/
 }
 
+// Function for scaling
+array2D matadd(const array2D &A, const array2D &B){
+
+    if (A.rows != B.rows || A.columns != B.columns){
+        std::cout << "incorrect dimensions with addition" << '\n';
+        assert(A.rows == B.rows || A.columns == B.columns);
+    }
+
+    array2D C(A.rows, B.columns);
+
+    for (size_t i = 0; i < C.rows; i++){
+        for (size_t j = 0; j < C.columns; j++){
+            C(i, j) = A(i, j) + B(i, j);
+        }
+    }
+
+    return C;
+}
+
+// Function for scaling
+array2D matdiff(const array2D &A, const array2D &B){
+
+    if (A.rows != B.rows || A.columns != B.columns){
+        std::cout << "incorrect dimensions with addition" << '\n';
+        assert(A.rows == B.rows || A.columns == B.columns);
+    }
+
+    array2D C(A.rows, B.columns);
+
+    for (size_t i = 0; i < C.rows; i++){
+        for (size_t j = 0; j < C.columns; j++){
+            C(i, j) = A(i, j) - B(i, j);
+        }
+    }
+
+    return C;
+}
+
+// Function for scaling
+array2D matscale(const array2D &A, double scale){
+
+    array2D C(A.rows, A.columns);
+
+    for (size_t i = 0; i < C.rows; i++){
+        for (size_t j = 0; j < C.columns; j++){
+            C(i, j) = A(i, j) * scale;
+        }
+    }
+
+    return C;
+}
+
+// Function for finding magnitude for a single columned array
+// Note: We should probably set this function to work for all types of matrices
+double matmag(const array2D &A){
+    double mag = 0;
+
+    if (A.columns > 1){
+        std::cout << "Not implemented yet for larger than 1 column." << '\n';
+        assert(A.columns < 1);
+    }
+
+    for (size_t i = 0; i < A.rows; i++){
+        mag += A(i,0)*A(i,0);
+    }
+
+    mag = sqrt(mag);
+    return mag;
+}
