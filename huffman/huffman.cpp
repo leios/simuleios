0a63d1266e46af4ccacbd33ea2b8c1b90c52aecf
leios <jrs.schloss@gmail.com>
2016-07-08 06:59:34 +0900

    Adding header file for later and visualization skeleton.

--- huffman/huffman.cpp
+++ huffman/huffman.cpp
@@ -13,71 +13,7 @@
 #include<queue>
 #include<string>
 #include<unordered_map>
-
-// Create the binary tree
-struct node{
-    char key;
-    double weight;
-
-    node() = default;
-    node(char k, double w){
-        key = k;
-        weight = w;
-    };
-    node *left;
-    node *right;
-    node *parent;
-};
-
-// Struct to compare nodes
-struct node_comparer{
-    bool operator()(const node *left, const node *right) const {
-        return left->weight > right->weight;
-    }
-};
-
-// Struct to hold bitvalues and also the length of the huffman tree
-// (huffman code points)
-struct huffman_cp{
-    // 32 is arbitrarily set
-    //std::bitset<128> code;
-    std::string code;
-    char key;
-};
-
-// Struct to hold final result before decoding
-struct huffman_tree{
-    node *root;
-    std::unordered_map<char, std::string> bitmap;
-    std::string phrase, encoded_phrase;
-};
-
-using node_queue = std::priority_queue<node*,std::vector<node*>,node_comparer>;
-
-// creates nodes from vectors of characters and doubles
-node_queue create_nodes(std::vector<char> keys, std::vector<double> weights);
-node_queue create_nodes(std::unordered_map<char, double> keyweights);
-
-// Creates the simple binary tree
-node* huffman(node_queue &initial_nodes);
-
-// creates bit code
-//std::vector<huffman_cp> create_bits(node* root);
-std::unordered_map<char, std::string> create_bits(node* root);
-
-// does the encoding
-std::string encode(std::unordered_map<char, std::string> bitmap, 
-                   std::string phrase);
-
-// Does a simple search
-void depth_first_search(node* root, huffman_cp &current,
-                                      std::vector<huffman_cp> &bitstrings);
-
-// Does a simple 2-pass encoding scheme
-huffman_tree two_pass_huffman(std::string phrase);
-
-// Simple decoding scheme
-void decode(huffman_tree encoded_tree);
+#include "huffman.h"
 
 /*----------------------------------------------------------------------------//
 * MAIN
---
+++ huffman/huffman_vis.cpp
@@ -0,0 +1,117 @@
+/*-------------huffman_vis.cpp------------------------------------------------//
+*
+* Purpose: To visualize a simple huffman tree for LeiosOS
+*
+*   Notes: This will be using the cairo package, hopefully creating animations
+*          I could use the subroutine-comic project, but this will be from 
+*          scratch
+*
+*-----------------------------------------------------------------------------*/
+
+#include <cairo.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <random>
+#include "huffman.h"
+
+#define num_frames 300
+//#define num_frames 20
+
+// Struct to hold positions
+struct pos{
+    double x, y;
+};
+
+// Struct for colors
+struct color{
+    double r, g, b;
+};
+
+
+// Struct to hold all the necessary data for animations
+struct frame{
+    int res_x, res_y;
+    int fps;
+    int curr_frame;
+    cairo_surface_t *frame_surface[num_frames];
+    cairo_t *frame_ctx[num_frames];
+    cairo_surface_t *bg_surface;
+    cairo_t *bg_ctx;
+    pos origin;
+    std::string pngbase;
+
+    // Function to call frame struct
+    frame(int x, int y, int ps, std::string pngname);
+
+    // Function to initialize the frame struct
+    void init(int r, int g, int b);
+
+    // Function to draw all frames in the frame struct
+    void draw_frames();
+
+    // Function to destroy all contexts and surfaces
+    void destroy_all();
+
+};
+
+/*----------------------------------------------------------------------------//
+* MAIN
+*-----------------------------------------------------------------------------*/
+
+int main(){
+
+} 
+
+// Function to initialize the frame struct
+void frame::init(int r, int g, int b){
+    int line_width = 3;
+    for (size_t i = 0; i < num_frames; ++i){
+        frame_surface[i] = 
+            cairo_image_surface_create(CAIRO_FORMAT_ARGB32, res_x, res_y);
+        frame_ctx[i] = cairo_create(frame_surface[i]);
+        cairo_set_source_rgb(frame_ctx[i],(double)r, (double)g, (double)b);
+        //cairo_rectangle(frame_ctx[i],0,0,res_x,res_y);
+        cairo_fill(frame_ctx[i]);
+        cairo_set_line_cap(frame_ctx[i], CAIRO_LINE_CAP_ROUND);
+        cairo_set_line_width(frame_ctx[i], line_width);
+        cairo_set_font_size(frame_ctx[i], 20.0);
+    }
+    bg_surface = 
+        cairo_image_surface_create(CAIRO_FORMAT_ARGB32, res_x, res_y);
+    bg_ctx = cairo_create(bg_surface);
+    curr_frame = 0;
+}
+
+// Function to draw all frames in the frame struct
+void frame::draw_frames(){
+    std::string pngid, number;
+    for (size_t i = 0; i < num_frames; ++i){
+        cairo_set_source_surface(frame_ctx[i], frame_surface[i], 0, 0);
+        cairo_paint(frame_ctx[i]);
+
+        // Setting up number with stringstream
+        std::stringstream ss;
+        ss << std::setw(5) << std::setfill('0') << i;
+        number = ss.str();
+
+        pngid = pngbase + number + ".png";
+        std::cout << pngid << '\n';
+        cairo_surface_write_to_png(frame_surface[i], pngid.c_str());
+    }
+
+}
+
+// Function to set the initial variables
+frame::frame(int x, int y, int ps, std::string pngname){
+    res_x = x;
+    res_y = y;
+    pngbase = pngname;
+    fps = ps;
+    origin.x = (double)x / 2.0;
+    origin.y = (double)y / 2.0;
+}
+
