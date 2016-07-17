0f4810239df04ec94bdf6b3b25bfc1dfd8e98811
leios <jrs.schloss@gmail.com>
2016-07-18 06:41:56 +0900

    adding in simple visualizationscheme of standard huffman tree.

--- huffman/huffman_vis.cpp
+++ huffman/huffman_vis.cpp
@@ -3,8 +3,11 @@
 * Purpose: To visualize a simple huffman tree for LeiosOS
 *
 *   Notes: This will be using the cairo package, hopefully creating animations
-*          I could use the subroutine-comic project, but this will be from 
-*          scratch
+*              I could use the subroutine-comic project, but this will be from 
+*              scratch
+*          In draw_tree, nodes are being overwritten
+*          draw_internal still need to be completed
+*          check y positions in draw_tree
 *
 *-----------------------------------------------------------------------------*/
 
@@ -20,11 +23,6 @@
 
 //#define num_frames 300
 #define num_frames 200
-
-// Struct to hold positions
-struct pos{
-    double x, y;
-};
 
 // Struct for colors
 struct color{
@@ -65,29 +63,26 @@ void grow_circle(frame &anim, double time, pos ori, double radius);
 
 // Function to animate a line from two points
 void animate_line(frame &anim, int start_frame, double time, 
-                  pos ori_1, double radius_1, pos ori_2, double radius_2);
+                  pos ori_1, pos ori_2);
 
 // Function to draw huffman tree
 void draw_external(frame &anim, double time, huffman_tree tree);
 
-// finding x pos for huffman tree drawing
-double find_x_pos(frame &anim, std::string bitstring);
-
 // Function to draw internal_nodes
 void draw_internal(frame &anim, double time, node_queue regenerated_nodes, 
-                   double radius, huffman_tree final_tree);
+                   huffman_tree final_tree);
 
 // Overloaded create_nodes function for root node
 // This time we are recreating the tree for later
 node_queue regenerate_nodes(frame &anim, node *root, 
                             std::unordered_map<char, std::string> bitmap);
 
-// Does a simple search to regenerated node_queue from root
-void depth_first_search(frame &anim, node* root, node_queue &regenerated_nodes,
-                        std::unordered_map<char, std::string> bitmap);
-
 // Function to draw layers
 void draw_layers(std::vector<frame> layer);
+
+// Function to get x and y positions of external nodes:
+void draw_tree(frame &anim, double x, node* root, int level, 
+               node_queue &regenerated_nodes);
 
 /*----------------------------------------------------------------------------//
 * MAIN
@@ -106,18 +101,28 @@ int main(){
     create_bg(layer[0], 0, 0, 0);
 
     // encoding with 2-pass huffman
-    huffman_tree final_tree = two_pass_huffman("Jack and Jill went up the hill to fetch a pail of water. Jack fell down and broke his crown and Jill came Tumbling after! \nWoo!");
+    huffman_tree final_tree = two_pass_huffman("Jack and Jill went up the hill to fetch a pail of water. Jack fell down and broke his crown and Jill came Tumbling after!");
     decode(final_tree);
 
     std::cout << "final_tree root weight is: " 
               << final_tree.root->weight << '\n';
 
-    node_queue regenerated_nodes = regenerate_nodes(layer[0],final_tree.root,
+    node_queue regenerated_nodes = regenerate_nodes(layer[1],final_tree.root,
                                                     final_tree.bitmap);
 
-    draw_external(layer[1], 10.0, final_tree);
+/*
+    // Destrying the regenerated_nodes and printing them out.
+    node *temp_node;
+    while (regenerated_nodes.size() > 0){
+        temp_node = regenerated_nodes.top();
+        regenerated_nodes.pop();
+        std::cout << temp_node->ori.x << '\t' << temp_node->ori.y << '\t' 
+                  << temp_node-> weight << '\n'; 
+    }
+*/
+
     layer[2].curr_frame = layer[1].curr_frame;
-    draw_internal(layer[2], 10.0, regenerated_nodes, 10, final_tree);
+    draw_internal(layer[2], 10.0, regenerated_nodes, final_tree);
 
     draw_layers(layer);
 
@@ -233,30 +238,13 @@ void grow_circle(frame &anim, double time, pos ori, double radius){
 
 // Function to animate a line from two points
 void animate_line(frame &anim, int start_frame, double time,  
-                  pos ori_1, double radius_1, pos ori_2, double radius_2){
+                  pos ori_1, pos ori_2){
 
     // Finding number of frames
     int draw_frames = time * anim.fps;
 
     // internal count that definitely starts at 0;
     int j = 0;
-
-    if (ori_1.x > ori_2.x){
-        ori_1.x -= radius_1;
-        ori_2.x += radius_2;
-    }
-    else{
-        ori_1.x += radius_1;
-        ori_2.x -= radius_2;
-    }
-    if (ori_1.y > ori_2.y){
-        ori_1.y -= radius_1;
-        ori_2.y += radius_2;
-    }
-    else{
-        ori_1.y += radius_1;
-        ori_2.y -= radius_2;
-    }
 
     double curr_x, curr_y;
 
@@ -285,131 +273,29 @@ void animate_line(frame &anim, int start_frame, double time,
 
 }
 
-// Function to draw huffman tree
-void draw_external(frame &anim, double time, huffman_tree tree){
-
-    // Creating positions for external nodes
-    std::vector<pos> external_nodes;
-    std::vector<std::string> characters;
-
-    double lowest_location = log10(1.0 / tree.root->weight);
-
-    pos temp_pos;
-
-    // Determining x and y positions
-    // NOTE: The .9 and .05 are arbitrary and should be modified in final form
-    for (auto key : tree.bitmap){
-        temp_pos.y = (log10(tree.weightmap[key.first] / tree.root->weight) 
-                      / lowest_location) 
-                     * .9 * anim.res_y + anim.res_y * 0.05;
-        temp_pos.x = find_x_pos(anim, key.second);
-        external_nodes.push_back(temp_pos);
-        characters.push_back({key.first});
-
-        std::cout << key.first << '\t' << temp_pos.x << '\t'
-                  << temp_pos.y << '\n';
-    }
-
-    std::cout << tree.bitmap.size() << '\t' << external_nodes.size() << '\n';
-
-    for (size_t i = 0; i < external_nodes.size(); ++i){
-        grow_circle(anim, time / (double)external_nodes.size(), 
-                    external_nodes[i], 10);
-
-        //temp_char[0] = characters[i];
-
-        // Placing text in circle
-        for (int j = anim.curr_frame; j < num_frames; ++j){
-            cairo_set_source_rgb(anim.frame_ctx[j], 0, 0, 0);
-            cairo_text_extents_t textbox;
-            cairo_text_extents(anim.frame_ctx[j], 
-                               characters[i].c_str(),
-                               &textbox);
-            cairo_move_to(anim.frame_ctx[j], 
-                          external_nodes[i].x - textbox.width / 2.0,
-                          external_nodes[i].y + textbox.height / 2.0);
-            cairo_show_text(anim.frame_ctx[j], characters[i].c_str());
-            cairo_stroke(anim.frame_ctx[j]);
-        }
-        
-    }
-
-}
-
-// finding x pos for huffman tree drawing
-double find_x_pos(frame &anim, std::string bitstring){
-
-    double x_pos = anim.res_x / 2.0;
-    for (size_t i = 0; i < bitstring.size(); ++i){
-        if (bitstring[i] == '0'){
-            //std::cout << "found a 0" << '\n';
-            x_pos += anim.res_x / pow(2,(double(i) + 2.0));
-        }
-       if (bitstring[i] == '1'){
-           //std::cout << "Found a 1" << '\n';
-           x_pos -= anim.res_x / pow(2,(double(i) + 2.0));
-       }
-
-       x_pos = ((x_pos - 0.5 * anim.res_x) * .95) + 0.5 * anim.res_x;
-    }
-
-    return x_pos;
-}
-
 // Function to draw internal_nodes
 void draw_internal(frame &anim, double time, node_queue regenerated_nodes, 
-                   double radius, huffman_tree final_tree){
+                   huffman_tree final_tree){
 
-    pos ori, ori_2;
-    double lowest_location = log10(1.0 / final_tree.root->weight);
-    double angle;
-    int num_lines = 30;
+    int num_lines = 40;
 
-    node *node1, *node2, *node_parent;
-    while (regenerated_nodes.size() > 1){
-        node1 = regenerated_nodes.top();
+    node *temp_node;
+
+    // Going through all regenerated nodes and drawing the lines to the
+    // internal nodes. Ignoring external nodes, which have been drawn in 
+    // the draw_tree function.
+    while (regenerated_nodes.size() > 0){
+        temp_node = regenerated_nodes.top();
         regenerated_nodes.pop();
-        node2 = regenerated_nodes.top();
-        regenerated_nodes.pop();
 
-        node_parent = new node();
-
-        node_parent->weight = node1->weight + node2->weight;
-        node_parent->x = (node1->x + node2->x) * 0.5;
-        node_parent->y = (log10(node_parent->weight /final_tree.root->weight)
-                          / lowest_location) 
-                         * .9 * anim.res_y + anim.res_y * 0.05;        
-        node1->y = (log10(node1->weight / final_tree.root->weight) 
-                    / lowest_location) 
-                    * .9 * anim.res_y + anim.res_y * 0.05;  
-        node2->y = (log10(node2->weight / final_tree.root->weight) 
-                    /lowest_location)
-                    * .9 * anim.res_y + anim.res_y * 0.05; 
-        node_parent->left = node1;
-        node_parent->right = node2;
-
-        // visualize this new node with lines...
-        // Drawing first line
-        ori.x = node1->x; ori.y = node1->y;
-        ori_2.x = node_parent->x; ori_2.y = node_parent->y;
-        if (node1->key){
-            angle = atan2((ori_2.y - ori.y), (ori_2.x - ori.x));
-            ori.x = ori.x + radius * cos(angle);
-            ori.y = ori.y + radius * sin(angle);
+        // Are we on an internal node?
+        if (!temp_node->key){
+            // Left line
+            animate_line(anim,anim.curr_frame,time/num_lines,
+                         temp_node->left->ori,temp_node->ori);
+            animate_line(anim,anim.curr_frame-((time/num_lines) * anim.fps)+1,
+                         time/num_lines,temp_node->right->ori,temp_node->ori);
         }
-        animate_line(anim,anim.curr_frame,time/num_lines,ori,0,ori_2,0);
-
-        ori.x = node2->x; ori.y = node2->y;
-        if (node2->key){
-            angle = atan2((ori_2.y - ori.y), (ori_2.x - ori.x));
-            ori.x = ori.x + radius * cos(angle);
-            ori.y = ori.y + radius * sin(angle);
-        }
-        animate_line(anim,anim.curr_frame-((time/num_lines) * anim.fps),
-                     time/num_lines,ori,0,ori_2,0);
-
-        regenerated_nodes.push(node_parent);
-
     }
 
 }
@@ -420,31 +306,48 @@ node_queue regenerate_nodes(frame &anim, node *root,
     node_queue regenerated_nodes;
 
     // Performs Depth-first search and push back the priorirty queue
-    depth_first_search(anim, root, regenerated_nodes, bitmap);
+    draw_tree(anim, anim.res_x * 0.5, root, 1, regenerated_nodes);
     
     return regenerated_nodes;
 }
 
-// Does a simple search to regenerated node_queue from root
-void depth_first_search(frame &anim, node* root, node_queue &regenerated_nodes,
-                        std::unordered_map<char, std::string> bitmap){
-
-    // Are we on a leaf?
-    if (!root->right && !root->left){
-        if (root->key){
-            std::string bitstring = bitmap[root->key];
-            root->x = find_x_pos(anim, bitstring);
-        }
-        // Filling the regenerated nodes 
-        regenerated_nodes.push(root);
-    }
+void draw_tree(frame &anim, double x, node* root, int level, 
+               node_queue &regenerated_nodes){
+    root->ori.x = x;
+    root->ori.y = level * anim.res_y / 8.25;
+    //root->ori.y = (1-(root->weight / 127.0)) * anim.res_y;
+    regenerated_nodes.push(root);
+    double dx = anim.res_x / pow(2, double(level) + 1);
+    //double dx = anim.res_x / 20;
 
     if (root->right){
-        depth_first_search(anim, root->right, regenerated_nodes, bitmap);
+        draw_tree(anim, x + dx, root->right, level + 1, regenerated_nodes);
     }
     if (root->left){
-        depth_first_search(anim, root->left, regenerated_nodes, bitmap);
+        draw_tree(anim, x - dx, root->left, level + 1, regenerated_nodes);
     }
+
+    if (!root->left && !root->right){
+        grow_circle(anim, 0.25, 
+                    root->ori, 10);
+
+        char test[] = { root->key, '\0' };
+
+        // Placing text in circle
+        for (int j = anim.curr_frame; j < num_frames; ++j){
+            cairo_set_source_rgb(anim.frame_ctx[j], 0, 0, 0);
+            cairo_text_extents_t textbox;
+            cairo_text_extents(anim.frame_ctx[j], 
+                               test,
+                               &textbox);
+            cairo_move_to(anim.frame_ctx[j], 
+                          root->ori.x - textbox.width / 2.0,
+                          root->ori.y + textbox.height / 2.0);
+            cairo_show_text(anim.frame_ctx[j], test);
+            cairo_stroke(anim.frame_ctx[j]);
+        }
+    }
+        
 
 }
 
