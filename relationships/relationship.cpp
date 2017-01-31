45392c2117e45eff02b177c7c866d4217efa173f
leios <jrs.schloss@gmail.com>
2017-02-01 06:52:49 +0900

    Adding mor functions for the shape matching relationship algorithm

--- relationships/relationship.cpp
+++ relationships/relationship.cpp
@@ -5,6 +5,7 @@
 *              https://en.wikipedia.org/wiki/Shape_context
 *
 *   Notes: Ideal relationship shape will be a heart (aww)
+*          Align shapes with hungarian algorithm, then match_shape
 *          
 *-----------------------------------------------------------------------------*/
 
@@ -26,19 +27,33 @@ std::vector<vec> add_blob(std::vector<vec> blob_1, std::vector<vec> blob_2);
 
 // Function to generate shape contexts
 // In principle, the shape contexts should be a vector of vector<double>
-std::vector<double> gen_shape_context(std::vector<vec> shape);
+std::vector<std::vector<double>> gen_shape_context(std::vector<vec> shape, 
+                                                   int res);
+
+// function to place all points in a grid related to the point's location
+std::vector<double> bin(vec point, std::vector<vec> shape, int res);
 
 // Function to align shapes
-std::vector<vec> align_shapes(std::vector<vec> shape_1, 
-                              std::vector<vec> shape_2);
+std::vector<vec> align_shapes(std::vector<std::vector<double>> context_1, 
+                              std::vector<std::vector<double>> context_2);
 
 // Function to compute the shape distance
-std::vector<double> gen_shape_dist(std::vector<double> context_1,
-                                   std::vector<double> context_2);
+std::vector<vec> gen_shape_dist(std::vector<vec> shape_1,
+                                std::vector<vec> shape_2);
+/*
+std::vector<vec> gen_shape_dist(std::vector<double> context_1,
+                                std::vector<double> context_2);
+*/
 
 // Function to modify the shape (relationship) to match the perfect shape
 std::vector<vec> match_shape(std::vector<vec> shape_1, 
                              std::vector<vec> shape_2);
+
+// Function to output shape_contexts
+void print_shape_context(std::vector<std::vector<double>> shape_context);
+
+// Function to use chi^2 test for a histogram distance measure
+double hist_dist(std::vector<double> hist_1, std::vector<double> hist_2);
 
 
 /*----------------------------------------------------------------------------//
@@ -57,16 +72,26 @@ int main(){
 
     std::vector<frame> layers = init_layers(3, res, fps, bg_clr);
 
-    std::vector<vec> heart = create_ideal_shape(100);
-    std::vector<vec> blob = create_blob(100);
+    std::vector<vec> heart = create_ideal_shape(10);
+    std::vector<vec> blob = create_blob(10);
 
     for (int i = 0; i < heart.size(); i++){
         std::cout << blob[i].x << '\t' << heart[i].y << '\n';
     }
 
+    int bin_res = 5;
+    std::vector<std::vector<double>> shape_context = 
+        gen_shape_context(heart, bin_res);
+
+    print_shape_context(shape_context);
+
     draw_array(layers[1], blob, 400, 400, line_clr);
 
     draw_layers(layers);
+
+    for (int i = 0; i < layers.size(); i++){
+        layers[i].destroy_all();
+    }
 
 }
 
@@ -143,4 +168,103 @@ std::vector<vec> add_blob(std::vector<vec> blob_1, std::vector<vec> blob_2){
 
     return blob_sum;
 
+}
+
+// Function to generate shape contexts
+// In principle, the shape contexts should be a vector of vector<double>
+std::vector<std::vector<double>> gen_shape_context(std::vector<vec> shape, 
+                                                   int res){
+
+    // Creating vector to work with
+    std::vector<std::vector<double>> shape_context(shape.size());
+
+    for (size_t i = 0; i < shape.size(); i++){
+        shape_context[i] = bin(shape[i], shape, res);
+    }
+
+    return shape_context;
+}
+
+// function to place all points in a grid related to the point's location
+// We can use any space we want for binning, try xy before log-polar
+std::vector<double> bin(vec point, std::vector<vec> shape, int res){
+
+    // Creating the vector to work with
+    std::vector<double> bin_data(res*res);
+
+    vec dist;
+    vec bin_dim = {2.0 / res, 2.0 / res};
+
+    // Bin position is the left / bottom -most corner
+    vec bin_pos = {-1,-1};
+    for (int i = 0; i < shape.size(); i++){
+
+        // Resetting bin_pos to -1, -1
+        bin_pos = {-1, -1};
+
+        dist = point - shape[i];
+        for (int j = 0; j < res; j++){
+            for (int k = 0; k < res; k++){
+                if (dist.x > bin_pos.x && dist.x < bin_pos.x + bin_dim.x &&
+                    dist.y > bin_pos.y && dist.y < bin_pos.y + bin_dim.y){
+                    bin_data[j + res*k] += 1.0;
+                    std::cout << "check " << i << '\n';
+                }
+                bin_pos.y += bin_dim.y;
+            }
+            bin_pos.x += bin_dim.x;
+
+            // Resetting bin_pos.y to -1 for loop
+            bin_pos.y = -1;
+        }
+    }
+
+    return bin_data;
+}
+
+// Function to output shape_contexts
+void print_shape_context(std::vector<std::vector<double>> shape_context){
+
+    int res = sqrt(shape_context[0].size());
+    int index = 0;
+    for (size_t i = 0; i < shape_context.size(); i++){
+        for (int j = 0; j < res; j++){
+            for (int k = 0; k < res; k++){
+                index = k + j*res;
+                std::cout << shape_context[i][index] << ' ';
+            }
+            std::cout << '\n';
+        }
+
+        std::cout << '\n';
+    }
+
+}
+
+// Function to compute the shape distance
+std::vector<vec> gen_shape_dist(std::vector<vec> shape_1,
+                                std::vector<vec> shape_2){
+
+    // Creating vector to work with
+    std::vector<vec> shape_dist(shape_1.size());
+
+    for(size_t i = 0; i < shape_dist.size(); i++){
+        shape_dist[i] = shape_1[i] - shape_2[i];
+    }
+
+    return shape_dist;
+}
+
+// Function to use chi^2 test for a histogram distance measure
+double hist_dist(std::vector<double> hist_1, std::vector<double> hist_2){
+
+    double distance = 0;
+
+    for (size_t i = 0; i < hist_1.size(); i++){
+        if (hist_1[i] + hist_2[i] != 0){
+            distance += 0.5*pow(hist_1[i] - hist_2[i], 2) 
+                             / (hist_1[i] + hist_2[i]);
+        }
+    }
+    return distance;
 }
