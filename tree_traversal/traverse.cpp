ac9f9860adefe406b6422d938e78c904ba332f04
leios <jrs.schloss@gmail.com>
2017-09-05 06:45:23 +0900

    adding method without pointers.

--- tree_traversal/traverse.cpp
+++ tree_traversal/traverse.cpp
@@ -7,19 +7,18 @@
 #include <iostream>
 #include <stack>
 #include <queue>
+#include <vector>
 
 struct node{
-    node *parent;
-    node **children;
-    int num_children;
+    std::vector<node> children;
     int ID;
 };
 
 // Function to create simple tree
-void create_tree(node* &root, int num_row, int num_child);
+void create_tree(node& root, int num_row, int num_child);
 
 // Function to do a depth-first search recursively
-void DFS_recursive(node* &root);
+void DFS_recursive(const node& root);
 
 // Function to do a depth-first search with a stack
 void DFS_stack();
@@ -30,32 +29,34 @@ void BFS_queue();
 int main(){
 
     // Creating tree
-    node *root;
-    create_tree(root, 3, 3);
+    node root;
+    create_tree(root, 3, 2);
     std::cout << "Tree created!" << '\n';
     DFS_recursive(root);
 
 }
 
-
 // Function to create simple tree
-void create_tree(node* &root, int num_row, int num_child){
-    root = new node;
-    root->ID = num_row;
-    root->num_children = num_child;
-    root->children = new node*[num_child];
-    if (num_row > 0){
-        for (int i = 0; i < num_child; ++i){
-            create_tree(root->children[i], num_row - 1, num_child);
-        }
+void create_tree(node& root, int num_row, int num_child){
+    root.ID = num_row;
+    if (num_row == 0){
+        return;
+    }
+    root.children.reserve(num_child);
+    for (int i = 0; i < num_child; ++i){
+        node child;
+        create_tree(child, num_row - 1, num_child);
+        root.children.push_back(child);
     }
 }
 
 // Function to do a depth-first search recursively
-void DFS_recursive(node* &root){
-    if (!root){return;}
-    std::cout << root->ID << '\n';
-    for (int i = 0; i < root->num_children; ++i){
-        DFS_recursive(root->children[i]);
+void DFS_recursive(const node& root){
+    if (root.children.size() == 0){
+        return;
+    }
+    std::cout << root.ID << '\n';
+    for (int i = 0; i < root.children.size(); ++i){
+        DFS_recursive(root.children[i]);
     }
 }
