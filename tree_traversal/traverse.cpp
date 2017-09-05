b64abb8c312670bb6c6fbcdadc6f5a8f3fde39b2
James Schloss <james.schloss@oist.jp>
2017-09-05 10:45:02 +0900

    adding traversal with stacks and queues

--- tree_traversal/traverse.cpp
+++ tree_traversal/traverse.cpp
@@ -21,18 +21,20 @@ void create_tree(node& root, int num_row, int num_child);
 void DFS_recursive(const node& root);
 
 // Function to do a depth-first search with a stack
-void DFS_stack();
+void DFS_stack(const node& root);
 
 // Function to do a breadth-first search with a queue
-void BFS_queue();
+void BFS_queue(const node& root);
 
 int main(){
 
     // Creating tree
     node root;
-    create_tree(root, 3, 2);
+    create_tree(root, 3, 3);
     std::cout << "Tree created!" << '\n';
-    DFS_recursive(root);
+    //DFS_recursive(root);
+    //DFS_stack(root);
+    BFS_queue(root);
 
 }
 
@@ -58,5 +60,38 @@ void DFS_recursive(const node& root){
     std::cout << root.ID << '\n';
     for (int i = 0; i < root.children.size(); ++i){
         DFS_recursive(root.children[i]);
+    }
+}
+
+// Function to do a depth-first search with a stack
+void DFS_stack(const node& root){
+
+    std::stack<node> s;
+    s.push(root);
+    node temp;
+
+    while (s.size() > 0){
+        std::cout << s.top().ID << '\n';
+        temp = s.top();
+        s.pop();
+        for (int i = 0; i < temp.children.size(); ++i){
+            s.push(temp.children[i]);
+        }
+    }
+}
+
+// Function to do a breadth-first search with a queue
+void BFS_queue(const node& root){
+    std::queue<node> q;
+    q.push(root);
+    node temp;
+
+    while (q.size() > 0){
+        std::cout << q.front().ID << '\n';
+        temp = q.front();
+        q.pop();
+        for (int i = 0; i < temp.children.size(); ++i){
+            q.push(temp.children[i]);
+        }
     }
 }
