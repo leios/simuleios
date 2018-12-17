be3d4fba2830137767fd20fe14dfa3edbf5599b1
leios <jrs.schloss@gmail.com>
2018-12-18 06:23:28 +0900

    Adding alpha channel and deprioritizing smaller points.

--- fractal/IFS.jl
+++ fractal/IFS.jl
@@ -13,18 +13,19 @@ end
 
 # Functions for fractal flames
 function linear(point::Array{Float64, 1}, c::Float64, color::Array{Float64, 1})
-    ret_point = zeros(5)
+    ret_point = zeros(6)
     ret_point[1] = point[1] * c
     ret_point[2] = point[2] * c
     ret_point[3] = color[1]
     ret_point[4] = color[2]
     ret_point[5] = color[3]
+    ret_point[6] = color[4]
 
     return ret_point
 end
 
 function heart(point::Array{Float64, 1}, color::Array{Float64, 1})
-    ret_point = zeros(5)
+    ret_point = zeros(6)
     r = sqrt(point[1]^2 + point[2]^2)
     theta = atan(point[2],point[1])
     ret_point[1] = r*(sin(r*theta))
@@ -32,75 +33,87 @@ function heart(point::Array{Float64, 1}, color::Array{Float64, 1})
     ret_point[3] = color[1]
     ret_point[4] = color[2]
     ret_point[5] = color[3]
+    ret_point[6] = color[4]
 
     return ret_point
 end
 
 function sinusoidal(point::Array{Float64, 1}, color::Array{Float64, 1})
-    ret_point = zeros(5)
+    ret_point = zeros(6)
     ret_point[1] = sin(point[1])
     ret_point[2] = cos(point[2])
     ret_point[3] = color[1]
     ret_point[4] = color[2]
     ret_point[5] = color[3]
+    ret_point[6] = color[4]
 
     return ret_point
 end
 
 function swirl(point::Array{Float64, 1}, color::Array{Float64, 1})
-    ret_point = zeros(5)
+    ret_point = zeros(6)
     r = sqrt(point[1]^2 + point[2]^2)
     ret_point[1] = point[1]*sin(r^2) - point[2]*cos(r^2)
     ret_point[2] = point[1]*cos(r^2) + point[2]*sin(r^2)
     ret_point[3] = color[1]
     ret_point[4] = color[2]
     ret_point[5] = color[3]
+    ret_point[6] = color[4]
 
     return ret_point
 end
 
-function sierpensky_flame(point::Array{Float64, 1}, chosen_function::Int64)
+function sierpensky_flame(point::Array{Float64, 1}, color::Array{Float64, 1},
+                          chosen_function::Int64)
     if (chosen_function == 1)
         point[1:2] *= 0.5
-        point[3] = 0.5
-        point[4] = 0.5
-        point[5] = 0.0
+        point[3] = color[1]
+        point[4] = color[2]
+        point[5] = color[3]
+        point[6] = color[4]
     elseif(chosen_function == 2)
         point[1:2] = [(point[1] - 1)/2, (point[2])/2]
-        point[3] = 0.0
-        point[4] = 0.5
-        point[5] = 0.5
+        point[3] = color[1]
+        point[4] = color[2]
+        point[5] = color[3]
+        point[6] = color[4]
     else
         point[1:2] = [(point[1])/2, (point[2]-1)/2]
-        point[3] = 0.5
-        point[4] = 0.0
-        point[5] = 0.5
+        point[3] = color[1]
+        point[4] = color[2]
+        point[5] = color[3]
+        point[6] = color[4]
     end
 
     return point
 end
 
-function barnsley_flame(point::Array{Float64, 1}, chosen_function::Int64)
+function barnsley_flame(point::Array{Float64, 1}, color::Array{Float64, 1},
+                        chosen_function::Int64)
     if (chosen_function == 1)
         point[1:2] = [0 0; 0 0.16]*point[1:2]
-        point[3] = 1.0
-        point[4] = 1.0
-        point[5] = 1.0
+        point[3] = color[1]
+        point[4] = color[2]
+        point[5] = color[3]
+        point[6] = color[4]
     elseif(chosen_function == 2)
         point[1:2] = [0.85 0.04; -0.04 0.85]*point[1:2] + [0, 1.60]
-        point[3] = 1.0
-        point[4] = 0.0
-        point[5] = 0.0
+        point[3] = color[1]
+        point[4] = color[2]
+        point[5] = color[3]
+        point[6] = color[4]
     elseif(chosen_function == 3)
         point[1:2] = [0.2 -0.26; 0.23 0.22]*point[1:2] + [0, 1.60]
-        point[3] = 0.0
-        point[4] = 1.0
-        point[5] = 0.0
+        point[3] = color[1]
+        point[4] = color[2]
+        point[5] = color[3]
+        point[6] = color[4]
     else
         point[1:2] = [-0.15 0.28; 0.26 0.24]*point[1:2] + [0, 0.44]
-        point[3] = 0.0
-        point[4] = 0.0
-        point[5] = 1.0
+        point[3] = color[1]
+        point[4] = color[2]
+        point[5] = color[3]
+        point[6] = color[4]
     end
 
     return point
@@ -108,10 +121,11 @@ function barnsley_flame(point::Array{Float64, 1}, chosen_function::Int64)
 end
 
 function write_image(p::Array{Pixel,2})
-    a = Array{RGB{Float64},2}(undef,size(p,1), size(p,2))
+    a = Array{RGB{N0f8},2}(undef,size(p,1), size(p,2))
     for i = 1:length(pixels)
-        a[i] = RGB{Float64}(pixels[i].color[1], pixels[i].color[2], 
-                            pixels[i].color[3])
+        a[i] = RGB{N0f8}(pixels[i].color[1],
+                         pixels[i].color[2], 
+                         pixels[i].color[3])
     end
     save("fractal.png", a)
 end
@@ -180,32 +194,33 @@ function flame(n::Int64, resx::Int64, resy::Int64,
         if (abs(r) < 0.0001)
             r = 0.0001
         end
-
 #=
         if (rnd <= 0.01)
-            point = barnsley_flame(point, 1)
+            point = barnsley_flame(point, [1.0, 1.0, 1.0, 1.0], 1)
         elseif(rnd > 0.01 && rnd <= 0.86)
-            point = barnsley_flame(point, 2)
+            point = barnsley_flame(point, [1.0, 0, 0, 1.0], 2)
         elseif(rnd > 0.86 && rnd <= 0.93)
-            point = barnsley_flame(point, 3)
+            point = barnsley_flame(point, [0, 1.0, 0, 1.0], 3)
         else
-            point = barnsley_flame(point, 4)
+            point = barnsley_flame(point, [0, 0, 1.0, 1.0], 4)
         end
 =#
-
         if (rnd <= 0.33)
-            #point = sierpensky_flame(point, 1)
-            point = sinusoidal(point, [0.0, 0.0, 1.0])
+            #point = sierpensky_flame(point, [0.5, 0.5, 0, 1], 1)
+            #point = linear(point, 0.5, [1.0, 0.0, 0.0, 1])
+            point = sinusoidal(point, [0.0, 0.0, 1.0, 1])
         elseif (rnd > 0.33 && rnd <= 0.66)
-            #point = sierpensky_flame(point, 2)
-            point = linear(point, 0.5, [1.0, 0.0, 0.0])
+            #point = sierpensky_flame(point, [0, 0.5, 0.5, 1], 2)
+            point = linear(point, 0.5, [1.0, 0.0, 0.0, 1])
         else
-            #point = sierpensky_flame(point, 3)
-            point = swirl(point, [1.0, 1.0, 1.0])
+            #point = sierpensky_flame(point, [0.5, 0, 0.5, 1], 3)
+            #point = linear(point, 0.5, [1.0, 0.0, 0.0, 1])
+            point = swirl(point, [1.0, 1.0, 1.0, 1])
         end
 
         # Final function attempts
-        final_point = heart(point, [1.0, 0.0, 1.0])
+        final_point = linear(point, .2, [0.0, 1.0, 0.0, 1])
+        #final_point = sinusoidal(final_point, [0.0, 0.0, 1.0])
 
         # blending the colors
         if (i > 20)
@@ -213,9 +228,9 @@ function flame(n::Int64, resx::Int64, resy::Int64,
             yval = Int(floor((final_point[2]+scaley*0.5) / (scaley / resy)))
             if (xval < resx && yval < resy && xval > 0 && yval > 0)
                 pixels[yval,xval].val += 1
-                pixels[yval,xval].color = (color+point[3:5])*0.5
+                pixels[yval,xval].color = (color+point[3:5]*point[6])*0.5
                 color = pixels[yval,xval].color
-                pixels[yval,xval].color = (color+final_point[3:5])*0.5
+                pixels[yval,xval].color = (color+final_point[3:5]*point[6])*0.5
             end
         end 
 
@@ -244,12 +259,10 @@ function flame(n::Int64, resx::Int64, resy::Int64,
     # Setting log plot and changing brightness
     for pixel in pixels
         pixel.val /= max
-        if (pixel.val > 0.0000001)
-            pixel.color = (pixel.color * pixel.val).^(1/gamma)
-            for c in pixel.color
-                if c > 1
-                    c = 1
-                end
+        pixel.color = (pixel.color * pixel.val).^(1/gamma)
+        for c in pixel.color
+            if c > 1
+                c = 1
             end
         end
     end
@@ -259,7 +272,7 @@ function flame(n::Int64, resx::Int64, resy::Int64,
 end
 
 #pixels = flame(100000000, 1000, 1000, 4., 4., 1.0)
-pixels = flame(1000000, 1000, 1000, 2., 2., 2.2)
+pixels = flame(1000000, 1000, 1000, 0.75, .75, 1.0)
 write_image(pixels)
 #sierpensky(1000000)
 #barnsley_fern(100000)
