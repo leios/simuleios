9037f1b756bcb92ec5cf22e221861f2b33361387
leios <jrs.schloss@gmail.com>
2019-01-09 07:09:35 +0900

    adding HSV domain coloring scheme

--- domain_coloring/domain_coloring.jl
+++ domain_coloring/domain_coloring.jl
@@ -1,35 +1,28 @@
 using Images, ImageMagick
 
-struct Pixel
-    r::Float64
-    g::Float64
-    b::Float64
-end
-
-function write_image(pixels::Array{Pixel, 2}, output_file::String)
+function write_image(pixels::Array{HSV, 2}, output_file::String)
     color_array = Array{RGB{N0f8},2}(undef, size(pixels)[1], size(pixels)[2])
 
     for i = 1:size(pixels)[1]
         for j = 1:size(pixels)[2]
-            color_array[i,j] = RGB{N0f8}(pixels[i,j].r,
-                                         pixels[i,j].g,
-                                         pixels[i,j].b)
+            color_array[i,j] = RGB(pixels[i,j])
         end
     end
-
+    
     save(output_file, color_array)
 end
 
-function simple_domain(res::Int64, xmax::Float64, output_file::String)
-    a = Array{Pixel,2}(undef, res, res)
+function simple_domain(res::Int64, xmax::Float64, alpha::Float64,
+                       output_file::String)
+    a = Array{HSV,2}(undef, res, res)
     for i = 1:res
         for j = 1:res
             x = -xmax + 2*i*xmax/res
             xi = -xmax + 2*j*xmax/res
             r = sqrt(x*x + xi*xi)
             theta = atan(xi,x)
-            a[i,j] = Pixel(r / (2*xmax), 0, (theta+pi) / (2*pi))
+            a[i,j] = HSV(((theta+pi) / (2*pi))*360, 1-alpha^r, 1)
         end 
     end 
-    write_image(a, "check.png")
+    write_image(a, output_file)
 end
