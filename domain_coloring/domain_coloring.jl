b060b092dabb8840408b6b2ef0f45d9019cc7e5c
leios <jrs.schloss@gmail.com>
2019-01-24 08:46:44 +0900

    grid example updated. Needs revision

--- domain_coloring/domain_coloring.jl
+++ domain_coloring/domain_coloring.jl
@@ -14,14 +14,19 @@ end
 
 function simple_domain(res::Int64, xmax::Float64, alpha::Float64,
                        output_file::String)
+    threshold = xmax / res
     a = Array{HSV,2}(undef, res, res)
     for i = 1:res
         for j = 1:res
             x = -xmax + 2*i*xmax/res
             xi = -xmax + 2*j*xmax/res
-            r = sqrt(x*x + xi*xi)
-            theta = atan(xi,x) + pi
-            a[i,j] = HSV((theta / (2*pi))*360, 1-alpha^r, 1)
+            if (abs(x%1) < threshold || abs(xi%1) < threshold)
+                a[i,j] = HSV(0,0,0)
+            else
+                r = sqrt(x*x + xi*xi)
+                theta = atan(xi,x) + pi
+                a[i,j] = HSV((theta / (2*pi))*360, 1-alpha^r, 1)
+            end
         end 
     end 
     write_image(a, output_file)
@@ -29,17 +34,24 @@ end
 
 function simple2_domain(res::Int64, xmax::Float64, alpha::Float64,
                         output_file::String)
+    threshold = xmax / res
     a = Array{HSV,2}(undef, res, res)
     for i = 1:res
         for j = 1:res
             x = -xmax + 2*i*xmax/res
             xi = -xmax + 2*j*xmax/res
-            r = x*x + xi*xi
+            r = sqrt(x*x + xi*xi)
             arg = 2*(atan(xi,x)+pi)
-            while (arg > 2*pi)
-                arg -= 2*pi
+            z = r*r*exp(arg*im)
+
+            if (abs(real(z))%1 < threshold || abs(imag(z))%1 < threshold)
+                a[i,j] = HSV(0,0,0)
+            else
+                while (arg > 2*pi)
+                    arg -= 2*pi
+                end
+                a[i,j] = HSV((arg / (2*pi))*360, 1-alpha^(r*r), 1)
             end
-            a[i,j] = HSV((arg / (2*pi))*360, 1-alpha^r, 1)
         end 
     end 
     write_image(a, output_file)
