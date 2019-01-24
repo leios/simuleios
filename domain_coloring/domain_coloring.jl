08404b136ed7d08d39b6d4191dd16da1e3cc3229
leios <jrs.schloss@gmail.com>
2019-01-25 06:56:08 +0900

    refactoring domain_coloring script

--- domain_coloring/domain_coloring.jl
+++ domain_coloring/domain_coloring.jl
@@ -12,48 +12,40 @@ function write_image(pixels::Array{HSV, 2}, output_file::String)
     save(output_file, color_array)
 end
 
-function simple_domain(res::Int64, xmax::Float64, alpha::Float64,
-                       output_file::String)
-    threshold = xmax / res
-    a = Array{HSV,2}(undef, res, res)
-    for i = 1:res
-        for j = 1:res
-            x = -xmax + 2*i*xmax/res
-            xi = -xmax + 2*j*xmax/res
-            if (abs(x%1) < threshold || abs(xi%1) < threshold)
-                a[i,j] = HSV(0,0,0)
-            else
-                r = sqrt(x*x + xi*xi)
-                theta = atan(xi,x) + pi
-                a[i,j] = HSV((theta / (2*pi))*360, 1-alpha^r, 1)
-            end
-        end 
-    end 
-    write_image(a, output_file)
+function f(r::Float64, theta::Float64)
+    return  r + theta*im
 end
 
-function simple2_domain(res::Int64, xmax::Float64, alpha::Float64,
-                        output_file::String)
-    threshold = xmax / res
+function f2(r::Float64, theta::Float64)
+    arg = 2*theta
+    while (arg > 2pi)
+        arg -= 2*pi
+    end
+    return r*r + arg*im
+end
+
+function draw_grid(pixel_color::HSV, z::Complex{Float64}, threshold::Float64)
+    z = real(z)*exp(imag(z)*im)
+    if (abs(real(z))%1 < threshold || abs(imag(z))%1 < threshold)
+        return HSV(0,0,0)
+    else
+        return pixel_color
+    end
+end
+
+function simple_domain(res::Int64, xmax::Float64, alpha::Float64,
+                       output_file::String, f, threshold)
     a = Array{HSV,2}(undef, res, res)
     for i = 1:res
         for j = 1:res
             x = -xmax + 2*i*xmax/res
             xi = -xmax + 2*j*xmax/res
             r = sqrt(x*x + xi*xi)
-            arg = 2*(atan(xi,x)+pi)
-            z = r*r*exp(arg*im)
-
-            if (abs(real(z))%1 < threshold || abs(imag(z))%1 < threshold)
-                a[i,j] = HSV(0,0,0)
-            else
-                while (arg > 2*pi)
-                    arg -= 2*pi
-                end
-                a[i,j] = HSV((arg / (2*pi))*360, 1-alpha^(r*r), 1)
-            end
+            theta = atan(xi,x) + pi
+            z = f(r, theta)
+            a[i,j] = HSV((imag(z) / (2*pi))*360, 1-alpha^(real(z)), 1)
+            a[i,j] = draw_grid(a[i,j], z, threshold)
         end 
     end 
     write_image(a, output_file)
 end
-
