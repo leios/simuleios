2bcfd34a0c478a208621870233532e85f23f0b4d
leios <jrs.schloss@gmail.com>
2019-03-07 07:01:07 +0900

    refactoring domain coloring

--- domain_coloring/domain_coloring.jl
+++ domain_coloring/domain_coloring.jl
@@ -12,35 +12,30 @@ function write_image(pixels::Array{HSV, 2}, output_file::String)
     save(output_file, color_array)
 end
 
-function f(r::Float64, theta::Float64)
-    return  r + theta*im
+function f(x::Complex{Float64})
+    r = sqrt(real(x)^2 + imag(x)^2)
+    theta = atan(imag(x), real(x))
+    complex_val = r*exp(theta*im)
+    return complex_val
 end
 
-function f2(r::Float64, theta::Float64)
-    arg = 2*theta
-    while (arg > 2pi)
-        arg -= 2*pi
-    end
-    return r*r + arg*im
+function f2(x::Complex{Float64})
+    r = sqrt(real(x)^2 + imag(x)^2)
+    theta = atan(imag(x), real(x)) 
+    complex_val = (r*exp(theta*im))^2
+    return complex_val
 end
 
-function poly(r::Float64, theta::Float64)
-    input_arg = 3*theta
-    while (input_arg > 2pi)
-        input_arg -= 2*pi
-    end
-    complex_val = r*r*r*exp(input_arg*im) + 1
-    magnitude = sqrt(abs2(complex_val))
-    arg = atan(imag(complex_val), real(complex_val)) + pi
-    while (arg > 2pi)
-        arg -= 2*pi
-    end
-    return magnitude + arg*im
+function poly(x::Complex{Float64})
+    r = sqrt(real(x)^2 + imag(x)^2)
+    theta = atan(imag(x), real(x)) 
+    complex_val = r*exp(theta*im)
+    return (complex_val)^3 + 3*complex_val
 end
+
 
 
 function draw_grid(pixel_color::HSV, z::Complex{Float64}, threshold::Float64)
-    z = real(z)*exp(imag(z)*im)
     if (abs(real(z))%1 < threshold || abs(imag(z))%1 < threshold)
         return HSV(0,0,0)
     else
@@ -53,13 +48,13 @@ function simple_domain(res::Int64, xmax::Float64, alpha::Float64,
     a = Array{HSV,2}(undef, res, res)
     for i = 1:res
         for j = 1:res
-            x = -xmax + 2*i*xmax/res
-            xi = -xmax + 2*j*xmax/res
-            r = sqrt(x*x + xi*xi)
-            theta = atan(xi,x) + pi
-            z = f(r, theta)
-            a[i,j] = HSV((imag(z) / (2*pi))*360, 1-alpha^(real(z)), 1)
-            #a[i,j] = draw_grid(a[i,j], z, threshold)
+            x = -xmax + 2*i*xmax/res + (-xmax + 2*j*xmax/res)im
+            z = f(x)
+            magnitude = abs(z)
+            argument = angle(z) + pi
+            
+            a[i,j] = HSV(argument/(2*pi)*360, 1-alpha^(magnitude), 1)
+            a[i,j] = draw_grid(a[i,j], z, threshold)
         end 
     end 
     write_image(a, output_file)
