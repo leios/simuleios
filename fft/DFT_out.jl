19142bd97ac51a3bb6da2f20bb2a8006db5511dd
leios <jrs.schloss@gmail.com>
2019-05-27 06:47:58 +0900

    fixing function title to accurately represent what they are actually plotting

--- fft/DFT_out.jl
+++ fft/DFT_out.jl
@@ -1,13 +1,14 @@
 using Images, ImageMagick
-function DFT_out(N::Int64, xmax::Float64, output_file::String)
-    n = [-xmax + 2*xmax*i/N for i =0:N-1]
-    k = append!(n[1:div(N,2)],n[div(N,2):N-1])'
-    transform_matrix = exp.(-2im*pi*n*k/N)
+function fourier_op_out(N::Int64, xmax::Float64, output_file::String)
+    n = [2*xmax * i / N for i = 1:N]
+    k = n'
+    #n = [-xmax + 2*xmax*i/N for i =0:N-1]
+    #k = append!(n[1:div(N,2)],n[div(N,2):N-1])'
+    transform_matrix = (exp.(-2im*pi*n*k/N).+(1.0+1im))/2
     a = Array{RGB{N0f8},2}(undef, N, N)
     for i = 1:N
         for j = 1:N
-            a[i,j] = RGB{N0f8}(abs(real(transform_matrix[i,j])), 0, abs(imag(transform_matrix[i,j])))
-            #a[i,j] = RGB{N0f8}(0, abs2(transform_matrix[i,j]), 0)
+            a[i,j] = RGB{N0f8}(real(transform_matrix[i,j]), 0, imag(transform_matrix[i,j]))
         end
     end
 
@@ -15,15 +16,14 @@ function DFT_out(N::Int64, xmax::Float64, output_file::String)
 
 end
 
-function FFT_out(N::Int64, output_file::String)
+function DFT_out(N::Int64, output_file::String)
     n = [i for i = 0:N-1]
     k = n'
-    transform_matrix = exp.(-2im*pi*n*k/N)
+    transform_matrix = (exp.(-2im*pi*n*k/N).+(1.0+1im))/2
     a = Array{RGB{N0f8},2}(undef, N, N)
     for i = 1:N
         for j = 1:N
-            a[i,j] = RGB{N0f8}(abs(real(transform_matrix[i,j])), 0, abs(imag(transform_matrix[i,j])))
-            #a[i,j] = RGB{N0f8}(0, abs2(transform_matrix[i,j]), 0)
+            a[i,j] = RGB{N0f8}(real(transform_matrix[i,j]), 0, imag(transform_matrix[i,j]))
         end
     end
 
