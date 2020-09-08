2b8bd971951a3e3a79221e795ba92eba1ab082d6
James Schloss <jrs.schloss@gmail.com>
2020-09-08 19:02:28 -0400

    missing find_seam function

--- seam_carving/seam_carving.jl
+++ seam_carving/seam_carving.jl
@@ -9,18 +9,47 @@ end
 
 # function to return magnitude of image elements
 # Todo: Maybe convert to another color-space for better magnitude
-function image_magnitude(img_element)
-    return sqrt(img_element.r^2 + img_element.g^2 + img_element.b^2)
+function brightness(img_element)
+    return img_element.r + img_element.g + img_element.b
 end
 
 # function to find energy of image
 function find_energy(img)
-    return abs.(imfilter(image_magnitude.(img), Kernel.sobel().*10))
+    energy_x = imfilter(brightness.(img), Kernel.sobel()[1])
+    energy_y = imfilter(brightness.(img), Kernel.sobel()[2])
+    return sqrt.(energy_x.^2 + energy_y.^2)
+end
+
+# function to find direction to move in
+# TODO: generalize to horizontal and vertical seams
+function find_energy_map(energy)
+    energy_map = zeros(size(energy))
+    energy_map[end,:] .= energy[end,:]
+    next_elements = zeros(Int, size(energy))
+
+    for i = size(energy)[1]-1:-1:1
+        for j = 1:size(energy)[2]
+            left = max(1, j-1)
+            right = min(size(energy)[2], j+1)
+
+            local_energy, next_element = findmin(energy_map[i+1, left:right])
+            energy_map[i,j] += local_energy + energy[i,j]
+            next_elements[i,j] = next_element - 2
+        end
+    end
+
+    return energy_map, next_elements
 end
 
 # function to create seams and return seam of minimum energy
 # n is number of seams
-function find_seam(energy, n, direction)
+function find_seam(energy, direction)
+
+    energy_map, next_elements = find_energy_map(energy)
+
+    seam = Seam(direction, zeros(size(energy[direction])))
+
+    
 end
 
 # function to remove seams
@@ -51,70 +80,6 @@ function remove_seam(img, seam::Seam)
     end
 
     return new_img
-end
-
-# This function removes a pixel from an image and saves 2 images:
-#     one with the pixel darkened
-#     another with the entire row of pixels shifted
-function color_pixel!(cimg, pixel::CartesianIndex, filebase, iteration)
-
-    # Image 1: setting the pixel black
-    println(cimg[pixel], typeof(cimg))
-    cimg[pixel] = RGB(1,1,1)
-
-    save(filebase*lpad(string(iteration-1),5,string(0))*".jpg", cimg)
-end
-
-function remove_pixel!(img, pixel::CartesianIndex, filebase, iteration)
-
-    # Image 2: shift the image
-    if pixel[2] > 1 && pixel[2] < size(img)[2]
-        img[pixel[1],:] = vcat(img[pixel[1], 1:pixel[2]-1],
-                               img[pixel[1], pixel[2]+1:end],
-                               RGB(0,0,0))
-    elseif pixel[2] == 1
-        img[pixel[1],:] = vcat(img[pixel[1],2:end], RGB(0,0,0))
-    elseif pixel[2] == size(img)[2]
-        img[pixel[1],:] = vcat(img[pixel[1],1:end-1], RGB(0,0,0))
-    end
-
-    save(filebase*lpad(string(iteration-1),5,string(0))*".jpg", img)
-
-end
-
-function naive_removal(img, n)
-    energy = find_energy(img)
-    colored_img = copy(img)
-
-    pixels = Array{CartesianIndex, 1}(undef,n)
-
-    for i = 1:n
-        pixel = argmin(energy)
-        colored_img[pixel] = RGB(0,0,0)
-        save("cout"*lpad(string(i-1),5,string(0))*".jpg", colored_img)
-
-        pixels[i] = pixel
-        energy[pixel] = 1000
-
-    end
-
-    removed_img = copy(colored_img)
-
-    for i = 1:n
-        if pixels[i][2] > 1 && pixels[i][2] < size(removed_img)[2]
-            removed_img[pixels[i][1],:] = vcat(removed_img[pixels[i][1], 1:pixels[i][2]-1],
-                                               removed_img[pixels[i][1], pixels[i][2]+1:end],
-                                               RGB(0,0,0))
-        elseif pixels[i][2] == 1
-            removed_img[pixels[i][1],:] = vcat(removed_img[pixels[i][1],2:end],
-                                               RGB(0,0,0))
-        elseif pixels[i][2] == size(img)[2]
-            removed_img[pixels[i][1],:] = vcat(removed_img[pixels[i][1],1:end-1], RGB(0,0,0))
-        end
-
-        save("rout"*lpad(string(i-1),5,string(0))*".jpg", removed_img)
-
-    end
 end
 
 # Putting it all together
