945746f864dcc679d8da6a772f2fa9d778f585dd
James Schloss <jrs.schloss@gmail.com>
2020-10-11 14:48:47 -0400

    adding 2d raytracing example with function for lenses

--- raytracing/2d.jl
+++ raytracing/2d.jl
@@ -16,6 +16,7 @@ mutable struct Ray
 
     # Set of positions
     positions::Array{Float64}
+
 end
 
 mutable struct Lens
@@ -25,17 +26,30 @@ mutable struct Lens
     # Lens radius
     r::Float64
 
-    # Index of refraction
-    ior::Float64
+    # Lens function
+    f
+end
+
+function constant(p)
+    return 1.5
 end
 
 function lens_normal_at(ray, lens)
-     return normalize(ray.p .- lens.p)
+    n = normalize(ray.p .- lens.p)
+    if dot(-n, ray.v) < 0
+        n *= -1
+    end
+
+    return n
 end
 
-function inside_of(ray, lens)
-    x = lens.p[1] - ray.p[1]
-    y = lens.p[2] - ray.p[2]
+function inside_of(ray::Ray, lens)
+    return inside_of(ray.p, lens)
+end
+
+function inside_of(pos, lens)
+    x = lens.p[1] - pos[1]
+    y = lens.p[2] - pos[2]
     if (x^2 + y^2 <= lens.r^2)
         return true
     else
@@ -46,17 +60,17 @@ end
 # note: light moves as a particular speed with respect to the medium it is
 #       moving through, so...
 #       n_2*v = n_1*l + (n_1*cos(theta_1) - n_2*cos(theta_2))*n
-#       Other approximations: r = n_1/n_2, c = -n*l
-function refract!(ray, lens)
+#       Other approximations: ior = n_1/n_2, c = -n*l
+function refract!(ray, lens, ior)
     n = lens_normal_at(ray, lens)
     c = dot(-n, ray.v);
-    d = 1.0 - lens.ior^2 * (1.0 - c^2);
+    d = 1.0 - ior^2 * (1.0 - c^2);
 
     if (d < 0.0)
-        return [0.0, 0.0];
+        return zeros(2);
     end
 
-    ray.v = lens.ior * ray.v + (lens.ior * c - sqrt(d)) * n;
+    ray.v = ior * ray.v + (ior * c - sqrt(d)) * n;
 end
 
 mutable struct Mirror
@@ -93,9 +107,10 @@ end
 
 function plot_rays(rays::Vector{Ray}, mirrors::Vector{Mirror},
                    lenses::Vector{Lens}, filename)
-    plt = plot()
+    plt = plot(background_color=:black, aspect_ratio=:equal)
     for ray in rays
-        plot!(plt, (ray.positions[:,1], ray.positions[:,2]); label = "ray")
+        plot!(plt, (ray.positions[:,1], ray.positions[:,2]); label = "ray",
+              linecolor=:white)
     end
 
     for mirror in mirrors
@@ -108,7 +123,7 @@ function plot_rays(rays::Vector{Ray}, mirrors::Vector{Mirror},
 
     for lens in lenses
         circle = draw_circle(lens.p, lens.r, 100)
-        plot!(circle, label="lens")
+        plot!(circle; label="lens", linecolor=:lightblue)
     end 
 
     savefig(plt, filename)
@@ -121,6 +136,7 @@ end
 function propagate!(rays::Vector{Ray}, mirrors::Vector{Mirror},
                     lenses::Vector{Lens}, n, dt)
 
+    ior = 1.0
     for i = 1:n
         for j = 1:length(rays)
             for mirror in mirrors
@@ -129,8 +145,20 @@ function propagate!(rays::Vector{Ray}, mirrors::Vector{Mirror},
                 end
             end
             for lens in lenses
-                if inside_of(rays[j], lens)
-                    refract!(rays[j], lens)
+                if inside_of(rays[j], lens) && i > 2
+                   if !inside_of(rays[j].positions[i-2, :], lens)
+                       ior = 1/lens.f(rays[j].p)
+                   elseif inside_of(rays[j].positions[i-2, :], lens)
+                       ior = lens.f(rays[j].positions[i-2,:])/lens.f(rays[j].p)
+                   end
+                   refract!(rays[j], lens, ior)
+                elseif !inside_of(rays[j], lens) && i > 2 &&
+                       inside_of(rays[j].positions[i-2, :], lens) 
+                    ior = lens.f(rays[j].positions[i-2, :])
+                    refract!(rays[j], lens, ior)
+                end
+                if rays[j].v == zeros(2)
+                    reflect!(rays[j], lens_normal_at(ray.p, lens))
                 end
             end
             step!(rays[j], dt)
@@ -141,11 +169,12 @@ end
 
 function parallel_propagate(ray_num, n; filename="check.png", dt = 0.1)
 
-    rays = [Ray([sqrt(0.5), sqrt(0.5)],
-            [float(i), 0.1],
+    rays = [Ray([1, 0],
+            [0.1, float(i)],
             zeros(n, 2)) for i = 1:ray_num]
 
-    lenses = [Lens([15, 10], 5, 1.5)]
+    lenses = [Lens([10, 6], 6, constant)]
+    #lenses = [Lens([-15, -10], 5, 1.5)]
     mirrors = [Mirror([0.0, -1.0],[5.0, 0.0])]
 #=
     mirrors = [Mirror([0.0, 1.0],[5.0, 5.0]),
