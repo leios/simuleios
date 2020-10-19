8aa3da227232030736150e380c7330734efac87a
James Schloss <jrs.schloss@gmail.com>
2020-10-19 17:46:35 -0400

    modifying timestepping approach such that each ray no longer has a vector inside it.

--- raytracing/2d_timestepping.jl
+++ raytracing/2d_timestepping.jl
@@ -1,6 +1,4 @@
 # TODO: allow for different shapes and mirror sizes
-# TODO: extend to variable refractive indices
-# TODO: Error in refract function
 
 using Plots
 using LinearAlgebra
@@ -8,15 +6,14 @@ using LinearAlgebra
 pyplot()
 
 mutable struct Ray
-    # Velocity vector
-    v::Vector{Float64}
+    # Direction vector
+    l::Vector{Float64}
 
     # Position vector
     p::Vector{Float64}
 
-    # Set of positions
-    positions::Array{Float64}
-
+    # Current Index of Refraction
+    ior::Float64
 end
 
 mutable struct Lens
@@ -45,7 +42,7 @@ end
 
 function lens_normal_at(ray, lens)
     n = normalize(ray.p .- lens.p)
-    if dot(-n, ray.v) < 0
+    if dot(-n, ray.l) < 0
         n *= -1
     end
 
@@ -66,20 +63,31 @@ function inside_of(pos, lens)
     end
 end
 
-# note: light moves as a particular speed with respect to the medium it is
-#       moving through, so...
-#       n_2*v = n_1*l + (n_1*cos(theta_1) - n_2*cos(theta_2))*n
-#       Other approximations: ior = n_1/n_2, c = -n*l
-function refract!(ray, lens, ior)
-    n = lens_normal_at(ray, lens)
-    c = dot(-n, ray.v);
+function refract!(ray, n)
+    c = dot(-n, ray.l);
     d = 1.0 - ior^2 * (1.0 - c^2);
 
     if (d < 0.0)
         return zeros(2);
     end
 
-    ray.v = ior * ray.v + (ior * c - sqrt(d)) * n;
+    ray.l = ior * ray.l + (ior * c - sqrt(d)) * n;
+end
+
+# note: light moves as a particular speed with respect to the medium it is
+#       moving through, so...
+#       n_2*v = n_1*l + (n_1*cos(theta_1) - n_2*cos(theta_2))*n
+#       Other approximations: ior = n_1/n_2, c = -n*l
+function refract!(ray, lens, ior)
+    n = lens_normal_at(ray, lens)
+    c = dot(-n, ray.l);
+    d = 1.0 - ior^2 * (1.0 - c^2);
+
+    if (d < 0.0)
+        return zeros(2);
+    end
+
+    ray.l = ior * ray.l + (ior * c - sqrt(d)) * n;
 end
 
 mutable struct Mirror
@@ -107,18 +115,18 @@ end
 #       In this case, the y component of l = cos(theta)*n
 #       so new vector: v = l + 2cos(theta)*n
 function reflect!(ray, n)
-    ray.v .-= 2*dot(ray.v, n).*n
+    ray.l .-= 2*dot(ray.l, n).*n
 end
 
 function draw_circle(p, r, res)
     return [x .+ (p[1], p[2]) for x in Plots.partialcircle(0, 2pi, res, r)]
 end
 
-function plot_rays(rays::Vector{Ray}, mirrors::Vector{Mirror},
+function plot_rays(positions, mirrors::Vector{Mirror},
                    lenses::Vector{Lens}, filename)
     plt = plot(background_color=:black, aspect_ratio=:equal)
-    for ray in rays
-        plot!(plt, (ray.positions[:,1], ray.positions[:,2]); label = "ray",
+    for i = 1:size(positions)[1]
+        plot!(plt, (positions[i,:,1], positions[i,:,2]); label = "ray",
               linecolor=:white)
     end
 
@@ -139,11 +147,11 @@ function plot_rays(rays::Vector{Ray}, mirrors::Vector{Mirror},
 end
 
 function step!(ray::Ray, dt)
-    ray.p .+= .+ ray.v.*dt
+    ray.p .+= .+ ray.l.*dt/ray.ior
 end
 
 function propagate!(rays::Vector{Ray}, mirrors::Vector{Mirror},
-                    lenses::Vector{Lens}, n, dt)
+                    lenses::Vector{Lens}, positions, n, dt)
 
     ior = 1.0
     for i = 1:n
@@ -155,24 +163,21 @@ function propagate!(rays::Vector{Ray}, mirrors::Vector{Mirror},
             end
             for lens in lenses
                 if inside_of(rays[j], lens) && i > 2
-                   if !inside_of(rays[j].positions[i-2, :], lens)
-                       ior = 1/lens.f(rays[j].p .- lens.p)
-                   elseif inside_of(rays[j].positions[i-2, :], lens)
-                       ior = lens.f(rays[j].positions[i-2,:] .- lens.p)/
-                             lens.f(rays[j].p .- lens.p)
-                   end
+                   ior = rays[j].ior/lens.f(rays[j].p .- lens.p)
+                   rays[j].ior = lens.f(rays[j].p .- lens.p)
                    refract!(rays[j], lens, ior)
                 elseif !inside_of(rays[j], lens) && i > 2 &&
-                       inside_of(rays[j].positions[i-2, :], lens) 
-                    ior = lens.f(rays[j].positions[i-2, :] .- lens.p)
+                       rays[j].ior != 1
+                    ior = rays[j].ior
+                    rays[j].ior = 1
                     refract!(rays[j], lens, ior)
                 end
-                if rays[j].v == zeros(2)
+                if rays[j].l == zeros(2)
                     reflect!(rays[j], lens_normal_at(rays[j], lens))
                 end
             end
             step!(rays[j], dt)
-            rays[j].positions[i,:] .= rays[j].p
+            positions[j, i,:] .= rays[j].p
         end
     end
 end
@@ -181,10 +186,10 @@ function parallel_propagate(ray_num, n; filename="check.png", dt = 0.1)
 
     rays = [Ray([1, 0],
             [0.1, float(i)],
-            zeros(n, 2)) for i = 1:ray_num]
+            1.0) for i = 1:ray_num]
 
     #lenses = [Lens([10, 6], 6, constant)]
-    lenses = [Lens([10, 6], 6, inverse)]
+    lenses = [Lens([10, 6], 6, constant)]
     #lenses = [Lens([-15, -10], 5, 1.5)]
     mirrors = [Mirror([0.0, -1.0],[5.0, 0.0])]
 #=
@@ -194,9 +199,11 @@ function parallel_propagate(ray_num, n; filename="check.png", dt = 0.1)
                Mirror([-1.0, 0.0],[10, 2.5])]
 =#
 
-    propagate!(rays, mirrors, lenses, n, dt)
+    positions = zeros(ray_num, n, 2)
 
-    plot_rays(rays, mirrors, lenses, filename)
+    propagate!(rays, mirrors, lenses, positions, n, dt)
+
+    plot_rays(positions, mirrors, lenses, filename)
 
     return rays
 end
