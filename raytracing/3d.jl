3fedb13912b427c864b1d59ee802fd377beeac4c
James Schloss <jrs.schloss@gmail.com>
2020-10-29 12:16:21 -0400

    adding functioning 3D example

--- raytracing/3d.jl
+++ raytracing/3d.jl
@@ -1,9 +1,7 @@
-# TODO: get refraction to work without offsets in quadratic / inside_of fxs
-#       Also, use dot product to see if exiting lens
-
 using Plots
 using LinearAlgebra
 using Test
+using FLoops
 
 pyplot()
 
@@ -30,7 +28,7 @@ end
 
 struct Ray
     # Velocity vector
-    v::Vector{Float64}
+    l::Vector{Float64}
 
     # Position vector
     p::Vector{Float64}
@@ -77,6 +75,30 @@ struct Sphere <: Object
     s::Surface
 end
 
+function sphere_rand(num_spheres, max_extents, max_radius)
+    spheres = [Sphere([0],0,Surface(0,0,0.,0)) for i = 1:num_spheres]
+    for i = 1:num_spheres
+        r = rand()
+        t = rand()
+        ca = rand()
+        all = r+t+ca
+        r = r / all
+        t = t / all
+        ca = ca / all
+        ior = 1 + rand()
+        sphere_surface = Surface(r, t, RGBA(rand(), rand(), rand(), ca), ior)
+        spheres[i] = Sphere([rand()*(max_extents[1, 2]-max_extents[1, 1])+
+                             max_extents[1, 1],
+                             rand()*(max_extents[2, 2]-max_extents[2, 1])+
+                             max_extents[2, 1],
+                             rand()*(max_extents[3, 2]-max_extents[3, 1])+
+                             max_extents[3, 1]],
+                            rand()*max_radius, sphere_surface)
+
+    end
+    return spheres
+end
+
 function Lens(p, r, ior)
     return Sphere(p, r, Surface(0,1,RGBA(0,0,0,0),ior))
 end
@@ -89,7 +111,7 @@ function ColoredSphere(p, r, c::RGB)
     return Sphere(p, r, Surface(0,0,RGBA(c), 0))
 end
 
-mutable struct SkyBox <: Object
+struct SkyBox <: Object
     # Skybox position
     p::Vector{Float64}
 
@@ -124,24 +146,23 @@ end
 function refract(ray, lens::Sphere, ior)
     n = sphere_normal_at(ray, lens)
 
-    if dot(n, ray.v) > 0
+    if dot(n, ray.l) > 0
         n .*= -1
     end
-    c = dot(-n, ray.v);
+    c = dot(-n, ray.l);
     d = 1.0 - ior^2 * (1.0 - c^2);
 
     if (d < 0.0)
-        reflect!(ray, n)
-        return
+        return reflect(ray, n)
     end
 
-    ray_vel = ior * ray.v + (ior * c - sqrt(d)) * n;
+    ray_vel = ior * ray.l + (ior * c - sqrt(d)) * n;
     return Ray(ray_vel, ray.p, ray.c)
 end
 
 abstract type Wall <: Object end;
 
-mutable struct Mirror <: Wall
+struct Mirror <: Wall
     # Normal vector
     n::Vector{Float64}
 
@@ -166,51 +187,17 @@ end
 #       In this case, the y component of l = cos(theta)*n
 #       so new vector: v = l + 2cos(theta)*n
 function reflect(ray, n)
-    ray_vel = ray.v .- 2*dot(ray.v, n).*n
-    ray_pos = ray.p .+ 0.001*ray.v
+    ray_vel = ray.l .- 2*dot(ray.l, n).*n
+    ray_pos = ray.p .+ 0.001*ray.l
     return Ray(ray_vel, ray_pos, ray.c)
-end
-
-function draw_circle(p, r, res)
-    return [x .+ (p[1], p[2]) for x in Plots.partialcircle(0, 2pi, res, r)]
-end
-
-function plot_rays(positions, objects::Vector{O},
-                   filename) where {O <: Object}
-    plt = plot(background_color=:black, aspect_ratio=:equal, legend=false)
-
-    for i = 1:size(positions)[1]
-        plot!(plt, (positions[i,:,1], positions[i,:,2]); label = "ray",
-              linecolor=:white)
-    end
-
-    for object in objects
-        if typeof(object) == Mirror
-            dir = [-object.n[2], object.n[1]]
-            extents = zeros(2,2)
-            extents[1,:] .= object.p .- object.scale * dir
-            extents[2,:] .= object.p .+ object.scale * dir
-            plot!(plt, (extents[:,1], extents[:,2]), label = "mirror")
-        elseif typeof(object) == Sphere
-            circle = draw_circle(object.p, object.r, 100)
-            plot!(circle; label="lens", linecolor=:lightblue)
-        end 
-    end
-
-    savefig(plt, filename)
-end
-
-function step(ray::Ray, dt)
-    ray.p .+= .+ ray.v.*dt
-    return ray
 end
 
 function intersection(ray::Ray, sphere::S;
                       threshold = 0.01) where
                       {S <: Union{Sphere, SkyBox}}
     relative_dist = ray.p-sphere.p
-    a = dot(ray.v, ray.v)
-    b = 2.0 * dot(relative_dist, ray.v)
+    a = dot(ray.l, ray.l)
+    b = 2.0 * dot(relative_dist, ray.l)
     c = dot(relative_dist, relative_dist) - sphere.r*sphere.r
     discriminant = b*b - 4*a*c
 
@@ -223,26 +210,26 @@ function intersection(ray::Ray, sphere::S;
         max = maximum(roots)
 
         if min > threshold
-            return (min)*ray.v
+            return (min)*ray.l
         elseif max > threshold
-            return (max)*ray.v
+            return (max)*ray.l
         else
             return nothing
         end
     else
         # Returns nothing if tangential
         return nothing
-        #return (-b/(2*a))*ray.v
+        #return (-b/(2*a))*ray.l
     end 
 end
 
 
 function intersection(ray::Ray, wall::W) where {W <: Wall}
-    intersection_pt = -dot((ray.p .- wall.p),wall.n)/dot(ray.v, wall.n)
+    intersection_pt = -dot((ray.p .- wall.p),wall.n)/dot(ray.l, wall.n)
 
     if isfinite(intersection_pt) && intersection_pt > 0 &&
        intersection_pt != NaN
-        return intersection_pt*ray.v
+        return intersection_pt*ray.l
     else
         return nothing
     end
@@ -250,7 +237,7 @@ end
 
 function propagate(rays::Array{Ray}, objects::Vector{O},
                     num_intersections) where {O <: Object}
-    for j = 1:length(rays)
+    @floop ThreadedEx() for j = 1:length(rays)
         rays[j] = propagate(rays[j], objects, num_intersections)
     end
 
@@ -261,7 +248,7 @@ function propagate(ray::Ray, objects::Vector{O},
                    num_intersections) where {O <: Object}
 
     for i = 1:num_intersections
-        if ray.v != zeros(length(ray.v))
+        if ray.l != zeros(length(ray.l))
             intersect_final = [Inf, Inf]
             intersected_object = nothing
             for object in objects
@@ -274,31 +261,48 @@ function propagate(ray::Ray, objects::Vector{O},
             end
 
             if intersect_final != nothing
-                ray.p .+= intersect_final
+                ray = Ray(ray.l, ray.p .+ intersect_final, ray.c)
                 if typeof(intersected_object) == Sphere
+                    reflected_ray = ray
+                    refracted_ray = ray
+                    colored_ray = ray
                     if !isapprox(intersected_object.s.t, 0)
                         ior = 1/intersected_object.s.ior
-                        if dot(ray.v,
+                        if dot(ray.l,
                                sphere_normal_at(ray,
                                                 intersected_object)) > 0
                             ior = intersected_object.s.ior
                         end
 
-                        ray = refract(ray, intersected_object, ior)
-                    elseif !isapprox(intersected_object.s.r, 0)
-                        n = sphere_normal_at(ray, intersected_object)
-                        ray = reflect(ray, n)
-                    elseif !isapprox(intersected_object.s.c.alpha, 0)
-                        ray_color = RGB(intersected_object.s.c)
-                        ray_vel = zeros(length(ray.v))
-                        ray = Ray(ray_vel, ray.p, ray_color)
+                        refracted_ray = refract(ray, intersected_object, ior)
+                        refracted_ray = propagate(refracted_ray, objects,
+                                                  num_intersections-i)
                     end
+
+                    if !isapprox(intersected_object.s.r, 0)
+                        n = sphere_normal_at(ray, intersected_object)
+                        reflected_ray = reflect(ray, n)
+                        reflected_ray = propagate(reflected_ray, objects,
+                                                  num_intersections-i)
+                    end
+
+                    if !isapprox(intersected_object.s.c.alpha, 0)
+                        ray_color = RGB(intersected_object.s.c)
+                        ray_vel = zeros(length(ray.l))
+                        colored_ray = Ray(ray_vel, ray.p, ray_color)
+                    end
+
+                    ray_color = intersected_object.s.t*refracted_ray.c +
+                                intersected_object.s.r*reflected_ray.c +
+                                intersected_object.s.c.alpha*colored_ray.c
+
+                    ray = Ray(zeros(length(ray.l)), ray.p, ray_color)
 
                 elseif typeof(intersected_object) == Mirror
                     ray = reflect(ray, intersected_object.n)
                 elseif typeof(intersected_object) == SkyBox
-                    ray_color = pixel_color(ray.p)
-                    ray_vel = zeros(length(ray.v))
+                    ray_color = pixel_color(ray.p, 1000)
+                    ray_vel = zeros(length(ray.l))
                     ray = Ray(ray_vel, ray.p, ray_color)
                 end
             else
@@ -310,8 +314,7 @@ function propagate(ray::Ray, objects::Vector{O},
     return ray
 end
 
-function pixel_color(position)
-    extents = 1000.0
+function pixel_color(position, extents)
     c = RGB(0)
     if position[1] < extents && position[1] > -extents
         c += RGB((position[1]+extents)/(2.0*extents), 0, 0)
@@ -383,17 +386,29 @@ end
 
 function main()
     sky = [SkyBox([0.0, 0.0, 0.0], 1000)]
-    spheres = [Lens([50,0,-25], 20, 1.5), ReflectingSphere([0,0,-25],20),
-               ColoredSphere([-50,0,-25], 20, RGB(0.25, 1, 0.75))]
+    spheres = [ColoredSphere([-50,0,-25], 20, RGB(0, 0, 1))]
+    #spheres = [Lens([0,0,-25], 20, 1.5), ReflectingSphere([0,50,-100],20),
+    #           ColoredSphere([-50,0,-25], 20, RGB(0, 0, 1))]
+#=
+    spheres = [Lens([0,0,-25], 20, 1.5), ReflectingSphere([0,50,-100],20),
+               ColoredSphere([-50,0,-25], 20, RGB(0, 0, 1)),
+               Sphere([30, 25, -60], 20,
+                      Surface(0.0, 0.75, RGBA(1,0,0,0.25), 1.5)),
+               Sphere([50, 0, -25], 20,
+                      Surface(0.5, 0.0, RGBA(0,1,0,0.5), 1.5)),
+               Sphere([-30, 25, -60], 20,
+                      Surface(0.5, 0.5, RGBA(1,1,1,0), 1.5))]
+=#
+    #bg_spheres = sphere_rand(10, [-400 400; -400 400; -350 -400], 50)
 
     objects = vcat(sky, spheres)
 
-    blank_img = Array{RGB}(undef, 1920,1080)
+    blank_img = Array{RGB}(undef, 1920, 1080)
     #blank_img = Array{RGB}(undef, 20, 10)
     blank_img[:] .= RGB(0)
 
-    cam = Camera(blank_img, [160,90], -100, [0,0,100])
+    cam = Camera(blank_img, [160,90], -100, [0,20,100])
 
-    return ray_trace(objects, cam)
+    return ray_trace(objects, cam; num_intersections=20)
 
 end
