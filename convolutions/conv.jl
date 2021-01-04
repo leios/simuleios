3a3ad2c49eca3bc6c3f8ca3a652e1cf6448f5af1
James Schloss <jrs.schloss@gmail.com>
2021-01-04 16:57:36 -0500

    Adding appropriate visualization and fixing convolution

--- convolutions/conv.jl
+++ convolutions/conv.jl
@@ -13,11 +13,11 @@ function signal_plot(i, signal1, signal2, out; filebase = "out",
     n1 = length(signal1)
     n2 = length(signal2)
     if n2 < n1
-        signal2 = [zeros(n1-n2);signal2]
+        signal2 = [reverse(signal2);zeros(n1-n2)]
     elseif n1 > n2
-        signal1 = [zeros(n2-n1);signal1]
+        signal1 = [signal1;zeros(n2-n1)]
     end
-    shift_signal = real([signal2[i:n1-1];signal2[1:i]])
+    shift_signal = real([signal2[n1-i:n1-1];signal2[1:n1-i]])
     conv_sum = real(signal1).*shift_signal
     plt = plot(real(signal1); label = "signal (f)", dpi=200)
     plt = plot!(plt, shift_signal; label = "filter (g)")
@@ -40,12 +40,12 @@ function conv_lin(signal1::Array{Complex{Float64},1},
 
     for i = 1:n-1
         if i+length(signal2) <= length(signal1)
-            rsum = sum(signal1[i:i+length(signal2)-1].*signal2)
+            rsum = sum(signal1[i:i+length(signal2)-1].*reverse(signal2))
         else
             offset = i+length(signal2)-length(signal1)
             println("offset is ", offset)
             rsum = sum(vcat(signal1[i:end], signal1[1:offset-1])
-                       .*signal2)
+                       .*reverse(signal2))
         end
         out[i] = rsum / norm_factor
         if plot
@@ -62,22 +62,11 @@ function main()
     x = zeros(Complex{Float64}, 100)
     x[40:60] .= 1.0 + 0im
     #y = [exp(-((i-50)/100)^2/.01) + 0im for i = 1:100]
-    y = [1.0 + 0im, 2.0, 3.0, 4.0, 5.0]
-    #normalize!(x)
+    y = [float(i)+0im for i = 19:-1:0]
+    println(typeof(y))
+    normalize!(x)
     normalize!(y)
     out_conv = conv_lin(x,y, norm_factor=sum(x.*x); plot=true)
-
-#=
-    x = zeros(Complex{Float64},100)
-    x[40:60] .= 1
-
-    y = copy(x)
-    y[40:60] .= [float(1-i*0.05) for i = 0:20]
-
-    # Make plots
-    out_corr = corr_plot(x,y;norm_factor=20; filebase = "corr_out")
-    out_conv = conv_plot(x,y, norm_factor=20)
-=#
 
     return x
 end
