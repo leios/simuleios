ea8975e1168f4fc00e373023556a889a6d3b46e5
James Schloss <jrs.schloss@gmail.com>
2021-11-03 16:54:12 +0100

    Adding working CPU variant, need to deal with warp comflicts

--- histograms/histograms.jl
+++ histograms/histograms.jl
@@ -4,7 +4,7 @@ using CUDAKernels
 using KernelAbstractions
 
 # Function to use as a baseline for CPU metrics
-function histogram(input)
+function create_histogram(input)
     histogram_output = zeros(Int, maximum(input))
     for i = 1:length(input)
         histogram_output[input[i]] += 1
@@ -16,70 +16,53 @@ end
 # The problem here is that the @synchronize calls are only synchronizing across
 #     blocks, while we need a way to combine all the shmem histograms together 
 #     into a big global blob. This can be done by:
-#     1. making sure we use lid instead of tid for the histogram at the end
-#        and use atomic adds
-#     2. lid doesn't make sense on the CPU with only 4 cores.
-#     3. fix error with jl_apply_type for GPU
-#     4. Think about svectors
-#     5. dynamic shmem allocation is not allowed, so use threadsize or a const
-#     6. We need to find a way to do a coalesced write to global memory
-#         - I think this will be easier on the GPU when more threads are avail
 @kernel function histogram_tag_kernel!(histogram_output, input)
     tid = @index(Global, Linear)
-    #lid = @index(Local, Linear)
+    lid = @index(Local, Linear)
 
-    #shared_histogram = @localmem Int32 length(histogram_output)
-    shared_histogram = @localmem Int64 128
+    @uniform gs = @groupsize()[1]
+    @uniform N = length(input)
 
-    # Note: this is 4 for CPU
-    #@print("LID is: ", lid, '\n')
-    @print("TID is: ", tid, '\n')
+    shared_histogram = @localmem Int64 gs
 
-    # TODO: anything but this!
-    if tid <= 128
-        @inbounds shared_histogram[tid] = 0
+    # Setting shared_histogram to 0
+    @inbounds shared_histogram[lid] = 0
+
+    @synchronize()
+
+    @uniform max_element = 1
+    for min_element = 1:gs:N
+        max_element = min_element + gs - 1
+        if max_element > N
+            max_element = N - min_element + 1
+        end
+
+        if input[tid] >= min_element && input[tid] <= max_element
+            @inbounds shared_histogram[input[tid]-min_element+1] += 1
+        end
+
+        @synchronize()
+        @inbounds histogram_output[lid+min_element-1] += shared_histogram[lid]
+        @inbounds shared_histogram[lid] = 0
     end
 
-    @synchronize()
-
-    #@print("values are: ", input[tid], "\n")
-
-    if tid < length(histogram_output)
-        @print("CHECK\n")
-    end 
-
-    if tid <= length(input)
-        @inbounds temp = shared_histogram[input[tid]]
-        @inbounds shared_histogram[input[tid]] = temp + 1
-        @print("input is: ", input[tid], '\n')
-        @print("hist is: ", shared_histogram[input[tid]], "\n\n")
-    end 
-
-    @synchronize()
-
-    @print(length(histogram_output), '\n')
-
-    if tid <= length(histogram_output)
-        @print("histogram values: ", shared_histogram[tid], '\n')
-        @inbounds histogram_output[input[tid]] += shared_histogram[input[tid]]
-    end
-
-    @synchronize()
 end
 
 function histogram_tag(input; numcores = 4, numthreads = 256)
     # I don't know how maximum is optimized on the GPU
-    histogram_output = zeros(Int64, maximum(input))
     if isa(input, Array)
+        histogram_output = zeros(Int64, maximum(input))
         event = histogram_tag!(histogram_output, input;
                                numcores = numcores, numthreads = numthreads)
         wait(event)
+        return histogram_output
     else
-        event = histogram_tag!(CuArray(histogram_output), input;
+        histogram_output = CuArray(zeros(Int64, maximum(input)))
+        event = histogram_tag!(histogram_output, input;
                                numcores = numcores, numthreads = numthreads)
         wait(event)
+        return histogram_output
     end
-    return histogram_output
 end
 
 function histogram_tag!(histogram_output, input;
